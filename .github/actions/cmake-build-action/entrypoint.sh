91c5e9ea42ee1f3b1034a4423c0ca1375f668485
Tim Niklas Uhl <tim.uhl@kit.edu>
2021-12-08 12:30:21 +0100

    stuff

--- .github/actions/cmake-build-action/entrypoint.sh
+++ .github/actions/cmake-build-action/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
-apt-get update
-apt-get install -y
-apt install libopenmpi-dev
+apt-get update > /dev/null
+apt-get install -y libopenmpi-dev
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
