f92943c74f3262e2e8402840f9629f94d6e9e592
Tim Niklas Uhl <tim.uhl@kit.edu>
2021-12-08 14:36:59 +0100

    Proof of concept

--- .github/actions/cmake-build-action/entrypoint.sh
+++ .github/actions/cmake-build-action/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
+echo "::group::Installing Open MPI"
 apt-get install -y libopenmpi-dev
+echo "::endgroup::"
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+export CC=gcc
+export CXX=g++
+export build_mode=Release
+cmake -B build -DCMAKE_BUILD_TYPE=$build_mode
+cmake --build build/
