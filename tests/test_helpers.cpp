196c0ab3fec3600245f483188a5f79148da31ac5
Demian Hespe <hespe@kit.edu>
2021-12-09 13:58:19 +0100

    Move EXPECT_NO_DEATH to helpers file

--- tests/test_helpers.cpp
+++ tests/test_helpers.cpp
@@ -8,26 +8,11 @@
 #include <gtest/gtest-death-test.h>
 #include <gtest/gtest.h>
 
+#include "gtest_helpers.hpp"
 #include "kamping/helpers.hpp"
 
 using namespace ::testing;
 
-///
-/// @brief Verifies that the expression passed does not crash
-///
-/// This works exiting with a 0 code after the expression and letting gtest check whether that exit occured.
-/// From https://stackoverflow.com/questions/60594487/expect-no-death-in-google-test
-///
-/// @param expression The expression that should succeed
-///
-#define EXPECT_NO_DEATH(expression)          \
-    EXPECT_EXIT(                             \
-        {                                    \
-            { #expression; }                 \
-            fprintf(stderr, "Still alive!"); \
-            exit(0);                         \
-        },                                   \
-        ::testing::ExitedWithCode(0), "Still alive!");
 
 TEST(HelpersTest, in_range) {
     uint8_t u8val = 200;
