585925cbc0beafdb53a73a655913e8239640b28d
Demian Hespe <hespe@kit.edu>
2021-12-09 13:55:13 +0100

    Add doxygen for EXPECT_NO_DEATH macro

--- tests/test_helpers.cpp
+++ tests/test_helpers.cpp
@@ -12,9 +12,14 @@
 
 using namespace ::testing;
 
-// From https://stackoverflow.com/questions/60594487/expect-no-death-in-google-test
-// This checks if the code after the expression was still executed by exiting afterwards and letting gtest check
-// whether the exit occurred
+///
+/// @brief Verifies that the expression passed does not crash
+///
+/// This works exiting with a 0 code after the expression and letting gtest check whether that exit occured.
+/// From https://stackoverflow.com/questions/60594487/expect-no-death-in-google-test
+///
+/// @param expression The expression that should succeed
+///
 #define EXPECT_NO_DEATH(expression)          \
     EXPECT_EXIT(                             \
         {                                    \
