3073702b0fc523bb0a587b6294a5d909014d8d2e
Demian Hespe <hespe@kit.edu>
2021-12-09 14:11:57 +0100

    Fix grammar mistake

--- tests/gtest_helpers.hpp
+++ tests/gtest_helpers.hpp
@@ -3,7 +3,7 @@
 ///
 /// @brief Verifies that the expression passed does not crash
 ///
-/// This works exiting with a 0 code after the expression and letting gtest check whether that exit occured.
+/// This works by exiting with a 0 code after the expression and letting gtest check whether that exit occured.
 /// From https://stackoverflow.com/questions/60594487/expect-no-death-in-google-test
 ///
 /// @param expression The expression that should succeed
