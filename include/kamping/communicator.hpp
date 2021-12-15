8c0f9a1ec8a4ddf553f6cf70142bffc80a493abd
Florian Kurpicz <florian@kurpicz.org>
2021-12-15 18:07:55 +0100

    Fix documentation for communicator

--- include/kamping/communicator.hpp
+++ include/kamping/communicator.hpp
@@ -4,33 +4,27 @@
 
 namespace kamping {
 
-  /// \brief Wrapper for MPI communicator providing access to \ref rank() and \ref size() of the communicator.
+  /// @brief Wrapper for MPI communicator providing access to \ref rank() and \ref size() of the communicator.
   class Communicator {
 
   public:
-    /// \brief Default constructor not specifying any MPI communicator and using \c MPI_COMMM_WORLD by default.
+    /// @brief Default constructor not specifying any MPI communicator and using \c MPI_COMMM_WORLD by default.
     Communicator() : Communicator(MPI_COMM_WORLD) { }
 
-    /// \brief Constructor where an MPI communicator has to be specified
-    explicit Communicator(MPI_Comm comm) : _comm(comm) {
-      MPI_Comm_rank(_comm, &_rank);
-      MPI_Comm_size(_comm, &_size);
-    }
-
-    /// \brief Rank of the current MPI process in the communicator.
-    /// \return Rank of the current MPI process in the communicator.
+    /// @brief Constructor where an MPI communicator has to be specified.
+    /// @param comm MPI communicator that is wrapped by this \c Communicator.
     int rank() const {
       return _rank;
     }
 
-    /// \brief Size of the communicator.
-    /// \return Size of the communicator.
+    /// @brief Size of the communicator.
+    /// @return Size of the communicator.
     int size() const {
       return _size;
     }
 
-    /// \brief MPI communicator corresponding to this communicator.
-    /// \return MPI communicator corresponding to this communicator.
+    /// @brief MPI communicator corresponding to this communicator.
+    /// @return MPI communicator corresponding to this communicator.
     MPI_Comm mpi_communicator() const {
       return _comm;
     }
