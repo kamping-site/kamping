5c79b0b350c2557d84eec1372cfa5b3624f46490
Matthias Schimek <matthias.schimek@kit.edu>
2022-01-02 21:50:20 +0100

    added some first tests

---
+++ tests/test_buffer_factories.cpp
@@ -0,0 +1,45 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2021 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+
+#include "kamping/buffer_factories.hpp"
+
+using namespace ::kamping;
+
+TEST(HelpersTest, send_buf_basics) {
+    std::vector<int>       int_vec{1, 2, 3, 4, 5, 6};
+    std::vector<int> const int_vec_const{1, 2, 3, 4, 5, 6};
+
+    auto gen_via_int_vec       = send_buf(int_vec);
+    auto gen_via_int_vec_const = send_buf(int_vec_const);
+
+    // value_type of a buffer should be the same as the value_type of the underlying container
+    static_assert(std::is_same_v<typename decltype(gen_via_int_vec)::value_type, int>);
+    static_assert(std::is_same_v<typename decltype(gen_via_int_vec_const)::value_type, int>);
+
+    // send buffers are always read-only as the do not need to be modified
+    EXPECT_FALSE(decltype(gen_via_int_vec)::is_modifiable);
+    EXPECT_FALSE(decltype(gen_via_int_vec_const)::is_modifiable);
+
+    EXPECT_EQ(decltype(gen_via_int_vec)::ptype, kamping::internal::ParameterType::send_buf);
+    EXPECT_EQ(decltype(gen_via_int_vec_const)::ptype, kamping::internal::ParameterType::send_buf);
+
+    const auto span_int_vec       = gen_via_int_vec.get();
+    const auto span_int_vec_const = gen_via_int_vec_const.get();
+
+    EXPECT_EQ(span_int_vec.ptr, int_vec.data());
+    EXPECT_EQ(span_int_vec_const.ptr, int_vec_const.data());
+    EXPECT_EQ(span_int_vec_const.size, 6);
+    EXPECT_EQ(span_int_vec_const.size, 6);
+}
---
+++ tests/test_buffers.cpp
@@ -0,0 +1,74 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2021 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include "kamping/buffers.hpp"
+
+using namespace ::kamping::internal;
+
+// Simplified Container to test KaMPI.ng's buffers with containers other than std::vector
+template <typename T>
+class OwnContainer {
+public:
+    using value_type = T;
+    T* data() {
+        return _vec.data();
+    }
+    const T* data() const noexcept {
+        return _vec.data();
+    }
+    std::size_t size() const {
+        return _vec.size();
+    }
+    void resize(std::size_t new_size) {
+        _vec.resize(new_size);
+    }
+
+private:
+    std::vector<T> _vec;
+};
+
+// Tests the basic functionality of ContainerBasedConstBuffer (i.e. its only public function get())
+TEST(ContainerBasedConstBuffer, get_basics) {
+    std::vector<int>       int_vec{1, 2, 3};
+    std::vector<int> const int_vec_const{1, 2, 3, 4};
+
+    constexpr ParameterType                            ptype = ParameterType::send_counts;
+    ContainerBasedConstBuffer<std::vector<int>, ptype> buffer_based_on_int_vector(int_vec);
+    ContainerBasedConstBuffer<std::vector<int>, ptype> buffer_based_on_const_int_vector(int_vec_const);
+
+    EXPECT_EQ(buffer_based_on_int_vector.get().size, int_vec.size());
+    EXPECT_EQ(buffer_based_on_int_vector.get().ptr, int_vec.data());
+    static_assert(std::is_same_v<decltype(buffer_based_on_int_vector.get().ptr), const int*>);
+
+    EXPECT_EQ(buffer_based_on_const_int_vector.get().size, int_vec_const.size());
+    EXPECT_EQ(buffer_based_on_const_int_vector.get().ptr, int_vec_const.data());
+    static_assert(std::is_same_v<decltype(buffer_based_on_const_int_vector.get().ptr), const int*>);
+}
+
+TEST(ContainerBasedConstBuffer, get_containers_other_than_vector) {
+    std::string                                         str = "I am underlying storage";
+    OwnContainer<int>                                   own_container;
+    constexpr ParameterType                             ptype = ParameterType::send_counts;
+    ContainerBasedConstBuffer<std::string, ptype>       buffer_based_on_string(str);
+    ContainerBasedConstBuffer<OwnContainer<int>, ptype> buffer_based_on_own_container(own_container);
+
+    EXPECT_EQ(buffer_based_on_string.get().size, str.size());
+    EXPECT_EQ(buffer_based_on_string.get().ptr, str.data());
+
+    EXPECT_EQ(buffer_based_on_own_container.get().size, own_container.size());
+    EXPECT_EQ(buffer_based_on_own_container.get().ptr, own_container.data());
+}
