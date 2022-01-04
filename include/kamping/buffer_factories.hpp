1d4152c2a186c3f6938128c681c82a8e698a52b0
Matthias Schimek <matthias.schimek@kit.edu>
2022-01-04 10:11:26 +0100

    add docu for factory functions

--- include/kamping/buffer_factories.hpp
+++ include/kamping/buffer_factories.hpp
@@ -24,7 +24,8 @@ namespace kamping {
 ///@brief Generates buffer wrapper based on a container for the send buffer, i.e. the underlying storage must contain
 /// the data elements to send.
 ///
-/// The underlying container must provide a \c data() member function and expose the contained \c value_type
+/// The underlying container must provide \c data() and \c size() member functions and expose the contained \c
+/// value_type
 ///@tparam Container Container type which contains the elements to send.
 ///@param container Container which contains the elements to send
 ///@return ContainerBasedConstBuffer refering to the storage containing the data elements to send.
@@ -36,7 +37,8 @@ internal::ContainerBasedConstBuffer<Container, internal::ParameterType::send_buf
 ///@brief Generates buffer wrapper based on a container for the send counts, i.e. the underlying storage must contain
 /// the send counts to each relevant PE.
 ///
-/// The underlying container must provide a \c data() member function and expose the contained \c value_type
+/// The underlying container must provide \c data() and \c size() member functions and expose the contained \c
+/// value_type
 ///@tparam Container Container type which contains the send counts.
 ///@param container Container which contains the send counts.
 ///@return ContainerBasedConstBuffer refering to the storage containing the send counts.
@@ -49,7 +51,8 @@ send_counts(const Container& container) {
 ///@brief Generates buffer wrapper based on a container for the recv counts, i.e. the underlying storage must contain
 /// the recv counts from each relevant PE.
 ///
-/// The underlying container must provide a \c data() member function and expose the contained \c value_type
+/// The underlying container must provide \c data() and \c size() member functions and expose the contained \c
+/// value_type
 ///@tparam Container Container type which contains the recv counts.
 ///@param container Container which contains the recv counts.
 ///@return ContainerBasedConstBuffer refering to the storage containing the recv counts.
@@ -62,7 +65,8 @@ recv_counts_in(const Container& container) {
 ///@brief Generates buffer wrapper based on a container for the send displacements, i.e. the underlying storage must
 /// contain the send displacements to each relevant PE.
 ///
-/// The underlying container must provide a \c data() member function and expose the contained \c value_type
+/// The underlying container must provide \c data() and \c size() member functions and expose the contained \c
+/// value_type
 ///@tparam Container Container type which contains the send displacements.
 ///@param container Container which contains the send displacements.
 ///@return ContainerBasedConstBuffer refering to the storage containing the send displacements.
@@ -75,7 +79,8 @@ send_displs_in(const Container& container) {
 ///@brief Generates buffer wrapper based on a container for the recv displacements, i.e. the underlying storage must
 /// contain the recv displacements from each relevant PE.
 ///
-/// The underlying container must provide a \c data() member function and expose the contained \c value_type
+/// The underlying container must provide \c data() and \c size() member functions and expose the contained \c
+/// value_type
 ///@tparam Container Container type which contains the recv displacements.
 ///@param container Container type which contains the recv displacements.
 ///@return ContainerBasedConstBuffer refering to the storage containing the recv displacements.
@@ -85,12 +90,11 @@ recv_displs_in(const Container& container) {
     return internal::ContainerBasedConstBuffer<Container, internal::ParameterType::recv_displs>(container);
 }
 
-// TODO adjust docu
 ///@brief Generates buffer wrapper based on a container for the receive buffer, i.e. the underlying storage
 /// will contained the received elements when the \c MPI call has been completed.
 /// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
 /// contained \c value_type
-///@tparam Container Container type which contains the recv displacements.
+///@tparam Container Container type which contains the received elements.
 ///@param container Container which will contain the received elements.
 ///@return UserAllocatedContainerBasedBuffer refering to the storage containing the received elements.
 template <typename Container>
@@ -105,36 +109,81 @@ recv_buf(NewContainer<Container>&&) {
     return internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_buf>();
 }
 
+///@brief Generates buffer wrapper based on a container for the send displacements, i.e. the underlying storage
+/// will contained the send displacements when the \c MPI call has been completed.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the send displacments.
+///@param container Container which will contain the send displacements.
+///@return UserAllocatedContainerBasedBuffer refering to the storage containing the send displacements.
 template <typename Container>
 internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::send_displs>
 send_displs_out(Container& container) {
     return internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::send_displs>(container);
 }
 
+///@brief Generates buffer wrapper based on a container for the send displacements, i.e. the underlying storage
+/// will contained the send displacements when the \c MPI call has been completed.
+/// The storage is allocated by the library and encapsulated in a container of type Container.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the send displacments.
+///@param container Container which will contain the send displacements.
+///@return LibAllocatedContainerBasedBuffer refering to the storage containing the send displacements.
 template <typename Container>
 internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::send_displs>
 send_displs_out(NewContainer<Container>&&) {
     return internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::send_displs>();
 }
 
+///@brief Generates buffer wrapper based on a container for the receive counts, i.e. the underlying storage
+/// will contained the receive counts when the \c MPI call has been completed.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the receive counts.
+///@param container Container which will contain the receive counts.
+///@return UserAllocatedContainerBasedBuffer refering to the storage containing the receive counts.
 template <typename Container>
 internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_counts>
 recv_counts_out(Container& container) {
     return internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_counts>(container);
 }
 
+///@brief Generates buffer wrapper based on a container for the receive counts, i.e. the underlying storage
+/// will contained the receive counts when the \c MPI call has been completed.
+/// The storage is allocated by the library and encapsulated in a container of type Container.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the send displacments.
+///@param container Container which will contain the receive counts.
+///@return LibAllocatedContainerBasedBuffer refering to the storage containing the receive counts.
 template <typename Container>
 internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_counts>
 recv_counts_out(NewContainer<Container>&&) {
     return internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_counts>();
 }
 
+///@brief Generates buffer wrapper based on a container for the receive displacements, i.e. the underlying storage
+/// will contained the receive displacements when the \c MPI call has been completed.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the receive displacements.
+///@param container Container which will contain the receive displacements.
+///@return UserAllocatedContainerBasedBuffer refering to the storage containing the receive displacements.
 template <typename Container>
 internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_displs>
 recv_displs_out(Container& container) {
     return internal::UserAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_displs>(container);
 }
 
+///@brief Generates buffer wrapper based on a container for the receive displacments, i.e. the underlying storage
+/// will contained the receive displacments when the \c MPI call has been completed.
+/// The storage is allocated by the library and encapsulated in a container of type Container.
+/// The underlying container must provide a \c data(), \c resize() and \c size() member function and expose the
+/// contained \c value_type
+///@tparam Container Container type which contains the send displacments.
+///@param container Container which will contain the receive displacments.
+///@return LibAllocatedContainerBasedBuffer refering to the storage containing the receive displacments.
 template <typename Container>
 internal::LibAllocatedContainerBasedBuffer<Container, internal::ParameterType::recv_displs>
 recv_displs_out(NewContainer<Container>&&) {
