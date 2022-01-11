06647c09c2a50dd93ea8b074ef9b30b8a603934e
Tim Niklas Uhl <tim.uhl@kit.edu>
2022-01-11 11:01:06 +0100

    Add license license information

--- tests/cmake/MPIGoogleTest.cmake
+++ tests/cmake/MPIGoogleTest.cmake
@@ -1,3 +1,6 @@
+# Adapted by the KaMPI.ng authors from GoogleTest.cmake included in CMake.
+#
+# Original license information:
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
