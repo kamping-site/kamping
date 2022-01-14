2e400b11ce580a4b03fe08e92b965028f03a8943
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-01-14 13:01:50 +0100

    Add tests for unary expressions

--- tests/test_kassert.cpp
+++ tests/test_kassert.cpp
@@ -21,6 +21,58 @@
 
 using namespace ::testing;
 
+TEST(KassertTest, unary_true_expressions) {
+    // unary expressions that evaluate to true and thus should not trigger the assertions
+
+    // literals
+    KASSERT(true);
+    KASSERT(!false);
+
+    // variables
+    const bool var_true = true;
+    const bool var_false = false;
+    KASSERT(var_true);
+    KASSERT(!var_false);
+
+    // function calls
+    auto id = [](const bool ans) {
+        return ans;
+    };
+    KASSERT(id(true));
+    KASSERT(!id(false));
+
+    // unary expressions with implicit conversion to true
+    KASSERT(10);
+    KASSERT(-10);
+    KASSERT(1 + 1); // unary expression from KASSERT perspective
+}
+
+TEST(KassertTest, unary_false_expressions) {
+    // test unary expressions that evaluate to false and should thus trigger the assertion
+
+    // literals
+    EXPECT_EXIT({ KASSERT(false); }, KilledBySignal(SIGABRT), "0");
+    EXPECT_EXIT({ KASSERT(!true); }, KilledBySignal(SIGABRT), "0");
+
+    // variables
+    const bool var_true = true;
+    const bool var_false = false;
+    EXPECT_EXIT({ KASSERT(var_false); }, KilledBySignal(SIGABRT), "0");
+    EXPECT_EXIT({ KASSERT(!var_true); }, KilledBySignal(SIGABRT), "0");
+
+    // functions
+    auto id = [](const bool ans) {
+        return ans;
+    };
+    EXPECT_EXIT({ KASSERT(id(false)); }, KilledBySignal(SIGABRT), "0");
+    EXPECT_EXIT({ KASSERT(!id(true)); }, KilledBySignal(SIGABRT), "0");
+
+    // expressions implicitly convertible to bool
+    EXPECT_EXIT({ KASSERT(0); }, KilledBySignal(SIGABRT), "0");
+    // EXPECT_EXIT({ KASSERT(nullptr); }, KilledBySignal(SIGABRT), "0"); -- std::nullptr_t is not convertible to bool
+    EXPECT_EXIT({ KASSERT(1 - 1); }, KilledBySignal(SIGABRT), "0"); // unary expression from KASSERT perspective
+}
+
 TEST(KassertTest, kassert_overloads_compile) {
     EXPECT_EXIT(
         { KASSERT(false, "__false_is_false_3__", kamping::assert::normal); }, KilledBySignal(SIGABRT),
