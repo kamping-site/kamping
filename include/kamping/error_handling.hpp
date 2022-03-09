d0faa44076549963aa776afb16ba20715226c5fd
Demian Hespe <hespe@kit.edu>
2022-03-09 16:03:10 +0100

    Add macro wrapper for MPI errors

--- include/kamping/error_handling.hpp
+++ include/kamping/error_handling.hpp
@@ -20,6 +20,16 @@
 
 #include <mpi.h>
 
+/// @brief Wrapper around KTHROW for MPI errors
+///
+/// Throws an MpiErrorException if the supplied error code is not MPI_SUCCESS
+///
+/// The macro accepts 2 parameters:
+/// 1. The error code returned by the MPI call (mandatory).
+/// 2. The MPI function that returned the error code (mandatory).
+#define THROW_IF_MPI_ERROR(error_code, function) \
+    KTHROW_SPECIFIED(error_code == MPI_SUCCESS, #function, MpiErrorException, error_code);
+
 namespace kamping {
 /// @brief The exception type used when an MPI call did not return MPI_SUCCESS.
 /// When using this with KTHROW you should call it like this: `KTHROW_SPECIFIED(err == MPI_SUCCESS, "<MPI function that
