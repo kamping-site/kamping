b5b96fc68ec19baf5b3893e3f4df9d1229511660
Demian Hespe <hespe@kit.edu>
2022-03-10 09:33:42 +0100

    Use correct copyright year

--- include/kamping/collectives/alltoall.hpp
+++ include/kamping/collectives/alltoall.hpp
@@ -1,6 +1,6 @@
 // This file is part of KaMPI.ng.
 //
-// Copyright 2021 The KaMPI.ng Authors
+// Copyright 2022 The KaMPI.ng Authors
 //
 // KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
