55629188ce33256ecbc80a85335b9e828f0f2649
Demian Hespe <Hespian@users.noreply.github.com>
2022-03-11 16:38:25 +0100

    Implement Alltoall (#132)
    * Implement alltoall
    
    * Add comments
    
    * Use CRTP
    
    * Remove unnecessary include
    
    * Move alltoall implementation to internal namespace
    
    * Write some first tests
    
    * add todo
    
    * Use new default parameter functions
    
    * Fix doxygen todo comment
    
    * Remove weird data type size calculations
    
    Apparently using different ones isn't even allowed according to the standard
    
    * Actually add tests
    
    * Add documentation
    
    * Naming
    
    * Commit commented out tests for mismatching types
    
    Just so they are somewhere in case we need them again
    
    * Remove commented out tests
    
    * Fix warning
    
    * Make the fix compile in debug mode
    
    * Add comment
    
    * Use custom container in test
    
    * Documentation changes
    
    * Make assertion light
    
    * Make pointer type more clear
    
    * Add todo for std::move(recv_buf)
    
    * Clean up includes
    
    * Add maybe_unused to avoid warning when exceptions are disabled
    
    * Remove unnecessary setting of assertion level and exception mode
    
    * Use correct copyright year
    
    * forward receive buffer into result
    
    * Use new error handling
    
    * Include array
    
    * Move include to correct include block
    
    * Use new name of KTHROW
    
    * Remove array include
    
    * Make move again
    
    ... and wait for #163 to ensure that that is actually allowed
    
    * Static assert that all parameters are rvalue references
    
    * Add error message
    
    * Change THROWING_KASSERT to KASSERT
    
    * Add todo comment

--- include/kamping/communicator.hpp
+++ include/kamping/communicator.hpp
@@ -16,13 +16,14 @@
 #include <mpi.h>
 
 #include "error_handling.hpp"
+#include "kamping/collectives/alltoall.hpp"
 #include "kamping/kassert.hpp"
 
 namespace kamping {
 
 /// @brief Wrapper for MPI communicator providing access to \ref rank() and \ref size() of the communicator. The \ref
 /// Communicator is also access point to all MPI communications provided by KaMPI.ng.
-class Communicator {
+class Communicator : public internal::Alltoall<Communicator> {
 public:
     /// @brief Default constructor not specifying any MPI communicator and using \c MPI_COMM_WORLD by default.
     Communicator() : Communicator(MPI_COMM_WORLD) {}
