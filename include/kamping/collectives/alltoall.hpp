5c4c021672af6af1f9dbc1ca321ebf1021c07f46
Daniel Seemaier <daniel@seemaier.de>
2022-03-25 09:13:32 +0100

    STL first, then other system headers, then KaMPI.ng headers

--- include/kamping/collectives/alltoall.hpp
+++ include/kamping/collectives/alltoall.hpp
@@ -13,16 +13,17 @@
 
 #pragma once
 
+#include <tuple>
+#include <type_traits>
+
+#include <mpi.h>
+
 #include "kamping/checking_casts.hpp"
 #include "kamping/kassert.hpp"
 #include "kamping/mpi_datatype.hpp"
 #include "kamping/mpi_function_wrapper_helpers.hpp"
 #include "kamping/named_parameter_selection.hpp"
 #include "kamping/parameter_factories.hpp"
-
-#include <mpi.h>
-#include <tuple>
-#include <type_traits>
 
 namespace kamping::internal {
 
