649dd5c8fe2b01be572342685c3af95e3003f912
Daniel Seemaier <daniel@seemaier.de>
2022-03-25 09:30:17 +0100

    .

--- include/kamping/collectives/alltoall.hpp
+++ include/kamping/collectives/alltoall.hpp
@@ -14,17 +14,15 @@
 #pragma once
 
 
-
-
 #include "kamping/checking_casts.hpp"
 #include "kamping/kassert.hpp"
 #include "kamping/mpi_datatype.hpp"
-#include <tuple>
-#include <type_traits>
 #include "kamping/mpi_function_wrapper_helpers.hpp"
 #include "kamping/named_parameter_selection.hpp"
 #include "kamping/parameter_factories.hpp"
 #include <mpi.h>
+#include <tuple>
+#include <type_traits>
 
 namespace kamping::internal {
 
