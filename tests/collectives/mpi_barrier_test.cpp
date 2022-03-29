10dabf047c27c9c0250c2b8ceabf414482249cf0
Lukas Hübner <lukas.huebner@klhuebner.de>
2022-03-29 10:35:04 +0200

    Auto calibrate the barrier() test's sleep duration.

--- tests/collectives/mpi_barrier_test.cpp
+++ tests/collectives/mpi_barrier_test.cpp
@@ -26,38 +26,64 @@ TEST(BarrierTest, barrier) {
     Communicator comm;
 
     // Test the given barrier implementation. Returns true, if the test passes, false otherwise.
-    auto test_the_barrier = [&comm](auto barrierImpl) -> bool {
-        const long sleep_for_ms = 10;
-
+    auto test_the_barrier = [&comm](auto barrierImpl, long sleep_for_ms) -> bool {
+        // All processes take the current time.
         MPI_Barrier(MPI_COMM_WORLD);
+        //! If we are unlucky, some processes exit this barrier more than sleep_for_ms after the root rank, which will
+        //! cause this test to fail, even for a valid barrier implementation.
         auto start = std::chrono::high_resolution_clock::now();
 
+        // The root process sleeps for a predefined amount of time, before entering the barrier.
         if (comm.is_root()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(sleep_for_ms));
         }
+        // All other processes enter the barrier immediately.
 
         barrierImpl();
 
+        // All processes check, if they spent at least the amount of time the root process slept inside the barrier.
         auto end                 = std::chrono::high_resolution_clock::now();
         auto time_diff           = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
         bool i_slept_long_enough = time_diff >= sleep_for_ms;
+
+        // We want to have the same result on all processes.
         bool everyone_slept_long_enough;
-        MPI_Reduce(
+        MPI_Allreduce(
             &i_slept_long_enough,        // send buffer
             &everyone_slept_long_enough, // receive buffer
             1,                           // count
             MPI_CXX_BOOL,                // datatype
             MPI_LAND,                    // operation
-            0,                           // root
             MPI_COMM_WORLD               // communicator
         );
         return everyone_slept_long_enough;
     };
 
-    EXPECT_FALSE(test_the_barrier([] { return; }));
-    EXPECT_TRUE(test_the_barrier([&comm] { comm.barrier(); }));
-    // This will not correctly detect all broken barrier implementations; e.g. the following will pass:
-    EXPECT_TRUE(test_the_barrier([] { std::this_thread::sleep_for(std::chrono::milliseconds(10)); }));
-    // On the other hand, detecting if a given implementation is a valid barrier implementation is equal to solving the
+    // If the scheduling is such, that the non-root processes are not scheduled for longer than the root process
+    // sleep()s, a broken barrier implementation might yield a false positive. We therefore have to test multiple sleep
+    // durations until the test fails.
+    bool test_failed  = false;
+    long sleep_for_ms = 10;
+    while (!test_failed) {
+        test_failed = !test_the_barrier([] { return; }, sleep_for_ms);
+        sleep_for_ms *= 2;
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    ASSERT_TRUE(test_failed);
+
+    // Even with this empirically determined sleep duration, we still get some false-negative test results for a valid
+    // barrier implementation. As this test can't be false positive, we can re-run it a given number of times or until
+    // it succeeds to get more reliable results. (See also the comment marked with ! above.)
+    const uint32_t max_tries      = 8;
+    bool           test_succeeded = false;
+    for (uint32_t i = 0; i < max_tries && !test_succeeded; ++i) {
+        test_succeeded = test_the_barrier([&comm] { comm.barrier(); }, sleep_for_ms);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    EXPECT_TRUE(test_succeeded);
+
+    // This will not correctly detect all broken barrier implementations; e.g. the following would pass:
+    // [] { std::this_thread::sleep_for(std::chrono::milliseconds(sleep_for_ms)); }
+    // On the other hand, detecting if a given function is a valid barrier implementation is equal to solving the
     // halting problem.
 }
