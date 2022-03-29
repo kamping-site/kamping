7af75b74c0b063c9f88d8afbfaf3806a2796fed3
Daniel Seemaier <daniel@seemaier.de>
2022-03-29 18:33:43 +0200

    Add compilation failure test

---
+++ tests/parameter_check_compilation_failures_test.cpp
@@ -0,0 +1,30 @@
+// This file is part of KaMPI.ng.
+//
+// Copyright 2022 The KaMPI.ng Authors
+//
+// KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPI.ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include <vector>
+
+#include "parameter_check_common.hpp"
+
+int main(int /* argc */, char** /* argv */) {
+#if defined(MISSING_REQUIRED_PARAMETER)
+    testing::test_required_send_buf();
+#elif defined(UNSUPPORTED_PARAMETER_NO_PARAMETERS)
+    std::vector<int> v;
+    testing::test_empty_arguments(kamping::send_buf(v));
+#elif defined(UNSUPPORTED_PARAMETER_ONLY_OPTIONAL_PARAMETERS)
+    std::vector<int> v;
+    testing::test_optional_recv_buf(kamping::send_buf(v));
+#else
+    // If none of the above sections is active, this file will compile successfully.
+#endif
+}
