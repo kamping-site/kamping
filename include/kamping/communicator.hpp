e45b5451fbcbd1e3892c40425cda14edd8d81c73
Lukas Hübner <lukashuebner@users.noreply.github.com>
2022-03-30 14:44:25 +0200

    Add barrier() wrapper for MPI_Barrier() (#206)
    * Add bcast() wrapper for MPI_Bcast()
    * Add an accompanying unit test
    * Add parameter pack to barrier() to speed up compile time if barrier() is unused.
    Thanks to Demian Hespe and Florian Kurpicz for reviewing.

--- include/kamping/communicator.hpp
+++ include/kamping/communicator.hpp
@@ -18,6 +18,7 @@
 
 #include "error_handling.hpp"
 #include "kamping/collectives/alltoall.hpp"
+#include "kamping/collectives/barrier.hpp"
 #include "kamping/collectives/gather.hpp"
 #include "kamping/collectives/reduce.hpp"
 #include "kamping/kassert.hpp"
@@ -27,8 +28,9 @@ namespace kamping {
 /// @brief Wrapper for MPI communicator providing access to \ref rank() and \ref size() of the communicator. The \ref
 /// Communicator is also access point to all MPI communications provided by KaMPI.ng.
 class Communicator : public internal::Alltoall<Communicator>,
+                     public internal::Reduce<Communicator>,
                      public internal::Gather<Communicator>,
-                     public internal::Reduce<Communicator> {
+                     public internal::Barrier<Communicator> {
 public:
     /// @brief Default constructor not specifying any MPI communicator and using \c MPI_COMM_WORLD by default.
     Communicator() : Communicator(MPI_COMM_WORLD) {}
