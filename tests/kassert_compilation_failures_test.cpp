cf14a47040254f1a29ce948b5e4874d577171b49
Daniel Seemaier <17594350+DanielSeemaier@users.noreply.github.com>
2022-03-30 17:08:35 +0200

    Fix forbid using && and || in KASSERT (#208)
    * Add section on short-circuit evaluation in KASSERT to coding guidelines.
    
    * Forbid && and || in KASSERT
    
    * Add compilation failure test
    
    Co-authored-by: Lukas Hübner <lukas.huebner@klhuebner.de>

---
+++ tests/kassert_compilation_failures_test.cpp
@@ -0,0 +1,26 @@
+// This file is part of KaMPI.ng.
+//
+// Copyright 2022 The KaMPI.ng Authors
+//
+// KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPI.ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include "kamping/kassert.hpp"
+
+#include "helpers_for_testing.hpp"
+
+int main(int /* argc */, char** /* argv */) {
+#if defined(FORBIDDEN_AND)
+    KASSERT(false && false);
+#elif defined(FORBIDDEN_OR)
+    KASSERT(false || false);
+#else
+    // If none of the above sections is active, this file will compile successfully.
+#endif
+}
