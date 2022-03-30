586495193a6c2b07a0ff572de8fe3e4d36f65fca
Lukas Hübner <lukas.huebner@klhuebner.de>
2022-03-30 11:16:52 +0200

    Add missing testing:: namespace in EXPECT_/ASSERT_KASSERT_FAILS

--- tests/helpers_for_testing.hpp
+++ tests/helpers_for_testing.hpp
@@ -75,10 +75,12 @@ struct Argument {
 };
 
 /// @brief Custom expectation for testing if a KASSERT fails.
-#define EXPECT_KASSERT_FAILS(CODE, FAILURE_MESSAGE) EXPECT_EXIT({ CODE; }, KilledBySignal(SIGABRT), FAILURE_MESSAGE);
+#define EXPECT_KASSERT_FAILS(CODE, FAILURE_MESSAGE) \
+    EXPECT_EXIT({ CODE; }, testing::KilledBySignal(SIGABRT), FAILURE_MESSAGE);
 
 /// @brief Custom assertion for testing if a KASSERT fails.
-#define ASSERT_KASSERT_FAILS(CODE, FAILURE_MESSAGE) ASSERT_EXIT({ CODE; }, KilledBySignal(SIGABRT), FAILURE_MESSAGE);
+#define ASSERT_KASSERT_FAILS(CODE, FAILURE_MESSAGE) \
+    ASSERT_EXIT({ CODE; }, testing::KilledBySignal(SIGABRT), FAILURE_MESSAGE);
 
 /// @}
 } // namespace testing
