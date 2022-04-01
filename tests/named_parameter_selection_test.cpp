69befdd933cea9c5b1bf6f818041a59b2bac289c
Daniel Seemaier <daniel@seemaier.de>
2022-04-01 11:24:50 +0200

    Fix compilation error if has_parameter_type is called in a function with zero arguments

--- tests/named_parameter_selection_test.cpp
+++ tests/named_parameter_selection_test.cpp
@@ -134,3 +134,13 @@ TEST(NamedParameterTest, all_parameters_are_rvalues) {
         EXPECT_TRUE(dummy_collective_operation(decltype(arg0){0}, decltype(arg1){1}));
     }
 }
+
+// Test that has_parameter_type can be invoked if the function is called with zero arguments
+template <typename... Args>
+bool dummy_test_has_parameter(Args&&... args [[maybe_unused]]) {
+    return has_parameter_type<ParameterType::send_buf, Args...>();
+}
+
+TEST(NamedParameterTest, has_parameter_on_empty_args) {
+    EXPECT_FALSE(dummy_test_has_parameter());
+}
