82c108f94d507f2b7b6dd1d776033f4fd87d1366
Demian Hespe <hespe@kit.edu>
2022-04-05 17:11:09 +0200

    Add test with 2 compilation units

---
+++ tests/multiple_compilation_units_test/gatherer.hpp
@@ -0,0 +1,28 @@
+// This file is part of KaMPI.ng.
+//
+// Copyright 2021-2022 The KaMPI.ng Authors
+//
+// KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPI.ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+/// @file
+/// @brief The header file for the second compilation unit of a test that checks if compiling and running works
+/// correctly when linking two compilation units that both use KaMPI.ng
+
+#include <vector>
+
+/// @brief A class that provides a gather function for single ints
+class Gatherer {
+public:
+    /// @brief Collective operation: Gather the ints provided on each rank on the root.
+    ///
+    /// @param data the int provided on this rank.
+    /// @return The gathered data on the root. An empty vector on all other ranks.
+    std::vector<int> gather(int data);
+};
---
+++ tests/multiple_compilation_units_test/main.cpp
@@ -0,0 +1,53 @@
+// This file is part of KaMPI.ng.
+//
+// Copyright 2021-2022 The KaMPI.ng Authors
+//
+// KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPI.ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+/// @file
+/// @brief The main file (and first compilation unit) of a test that checks if compiling and running works correctly
+/// when linking two compilation units that both use KaMPI.ng
+
+#include <cstddef>
+
+#include <mpi.h>
+
+#include "./gatherer.hpp"
+#include "kamping/communicator.hpp"
+
+/// @brief The main function for this tests. Gathers the ranks on the root (done in a different compilation unit), calls
+/// a barrier and checks the result
+///
+/// @param argc The number of command line arguments
+/// @param argv The command line arguments
+int main(int argc, char** argv) {
+    using namespace kamping;
+
+    MPI_Init(&argc, &argv);
+    Communicator comm;
+
+    Gatherer gatherer;
+    auto     gathered_data = gatherer.gather(comm.rank());
+
+    comm.barrier();
+
+    // Using assert here because this is supposed to do thing the way a normal KaMPI.ng application would do it.
+    // Since KASSERT is a KaMPI.ng internal, we cannot use it here.
+    if (comm.rank() == 0) {
+        assert(gathered_data.size() == static_cast<size_t>(comm.size()));
+        for (int rank = 0; rank < comm.size(); ++rank) {
+            assert(gathered_data[static_cast<size_t>(rank)] == rank);
+        }
+    } else {
+        assert(gathered_data.empty());
+    }
+
+    return 0;
+}
