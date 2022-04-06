f555bf25509c16843519f6beeb57ae1a86216224
Daniel Seemaier <daniel@seemaier.de>
2022-04-06 22:25:28 +0200

    implement review

--- .githooks/install.sh
+++ .githooks/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Get the absolute location of this script 
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
@@ -14,41 +14,37 @@ DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 HOOKS="${DIR}/../.git/hooks"
 
 # Print diff of new hooks
-find "${DIR}" -type f \( ! -name "*.*" \) |
-	while read hook_inactive_path; do
-		hook=$(basename "$hook_inactive_path")
-		hook_active_path="${HOOKS}/${hook}"
+for hook_inactive_path in $(find "${DIR}" -type f \( ! -name "*.*" \)); do 
+	hook=$(basename "$hook_inactive_path")
+	hook_active_path="${HOOKS}/${hook}"
 
-		if [[ -f "$hook_active_path" ]]; then # Hook already active 
-			if ! cmp -s "$hook_inactive_path" "$hook_active_path"; then # Hook changed
-				echo "################################################################################"
-				echo "# Changed hook '$hook'"
-				echo "################################################################################"
-				diff "$hook_inactive_path" "$hook_active_path"
-				echo ""
-			fi
-		else # New hook
+	if [[ -f "$hook_active_path" ]]; then # Hook already active 
+		if ! cmp --silent "$hook_inactive_path" "$hook_active_path"; then # Hook changed
 			echo "################################################################################"
-			echo "# New hook '$hook'"
+			echo "# Changed hook '$hook'"
 			echo "################################################################################"
-			cat "$hook_inactive_path"
+			diff "$hook_inactive_path" "$hook_active_path"
 			echo ""
+		else # No changes -> skip file 
+			continue 
 		fi
-	done
+	else # New hook
+		echo "################################################################################"
+		echo "# New hook '$hook'"
+		echo "################################################################################"
+		cat "$hook_inactive_path"
+		echo ""
+	fi
 
-echo "################################################################################"
-echo "Do you want these changes to become active?"
-select ans in "Yes" "No"; do 
-	case $ans in
-		Yes) 
-			find "${DIR}" -type f \( ! -name "*.*" \) |
-				while read hook_inactive_path; do 
-					hook=$(basename "$hook_inactive_path")
-					hook_active_path="${HOOKS}/${hook}"
-					cp "$hook_inactive_path" "$hook_active_path"
-				done
-			exit;;
-		No)
-			exit;;
-	esac
+	echo "Do you want these changes to become active?"
+	select answer in "Yes" "No"; do 
+		case $answer in
+			Yes) 
+				cp "$hook_inactive_path" "$hook_active_path"
+				break;;
+			No)
+				break;;
+		esac
+	done
 done
+
