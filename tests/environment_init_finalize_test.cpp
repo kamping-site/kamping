37f369509dc373a388e23b4cbfb52cb5b1ad97de
Demian Hespe <hespe@kit.edu>
2022-04-07 14:26:57 +0200

    Add tests

---
+++ tests/environment_init_finalize_test.cpp
@@ -0,0 +1,43 @@
+// This file is part of KaMPI.ng.
+//
+// Copyright 2022 The KaMPI.ng Authors
+//
+// KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPI.ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include <mpi.h>
+
+#include "kamping/environment.hpp"
+#include "kamping/kassert.hpp"
+
+using namespace ::kamping;
+
+// This is not using google test because our test setup would call MPI_Init before running any tests
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    Environment<false> utiliy_environment;
+    KASSERT(!utiliy_environment.initialized());
+    KASSERT(!utiliy_environment.finalized());
+    {
+#if defined(KAMPING_ENVIRONMENT_TEST_NO_PARAM)
+        Environment environment;
+#elif defined(KAMPING_ENVIRONMENT_TEST_WITH_PARAM)
+        Environment environment(argc, argv);
+#else
+        static_assert(false, "Define either KAMPING_ENVIRONMENT_TEST_NO_PARAM or KAMPING_ENVIRONMENT_TEST_WITH_PARAM");
+#endif
+
+        KASSERT(environment.initialized());
+        KASSERT(!environment.finalized());
+#if defined(KAMPING_ENVIRONMENT_TEST_EXPLICIT_FINALIZE)
+        environment.finalize();
+        KASSERT(environment.finalized());
+#endif
+    }
+    KASSERT(utiliy_environment.finalized());
+}
