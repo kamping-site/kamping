8ffc00437103e806458eed7f473361a246c9e177
Demian Hespe <hespe@kit.edu>
2022-05-10 10:17:27 +0200

    Fix name in new file

--- tests/environment_test.cpp
+++ tests/environment_test.cpp
@@ -1,6 +1,6 @@
-// This file is part of KaMPI.ng.
+// This file is part of KaMPIng.
 //
-// Copyright 2022 The KaMPI.ng Authors
+// Copyright 2022 The KaMPIng Authors
 //
 // KaMPI.ng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
