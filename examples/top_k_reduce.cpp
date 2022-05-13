c3aa4590479c2c313ce98916e9de22063582adb6
Niklas Uhl <niklas.uhl@online.de>
2022-05-13 14:48:18 +0200

    Fix bug

--- examples/top_k_reduce.cpp
+++ examples/top_k_reduce.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cstddef>
+#include <optional>
 #include <ostream>
 
 #include <mpi.h>
@@ -27,34 +28,20 @@ private:
 };
 
 template <size_t K, typename ValueType>
-TopK<K, ValueType> merge_top_k(TopK<K, ValueType> const& lhs, TopK<K, ValueType> const& rhs) {
-    size_t          lhs_current = 0;
-    size_t          rhs_current = 0;
-    TopK<K, size_t> merged;
-    for (size_t i = 0; i < K; i++) {
-        if (lhs[lhs_current] < rhs[rhs_current]) {
-            merged[i] = lhs[lhs_current];
-            lhs_current++;
-        } else {
-            merged[i] = rhs[rhs_current];
-            rhs_current++;
-        }
-    }
-    return merged;
-}
-
-template <size_t K, typename ValueType>
 std::ostream& operator<<(std::ostream& os, TopK<K, ValueType> const& top_k) {
     os << "TopK(";
     for (size_t i = 0; i < K; ++i) {
-        os << top_k[i] << ", ";
+        if (i != 0) {
+            os << ", ";
+        }
+        os << top_k[i];
     }
     os << ")";
     return os;
 }
 
 template <size_t K, typename ValueType>
-TopK<K, ValueType> kamping_top_k(TopK<K, ValueType> const& local_top_k, kamping::Communicator& comm) {
+std::optional<TopK<K, ValueType>> kamping_top_k(TopK<K, ValueType> const& local_top_k, kamping::Communicator& comm) {
     using namespace kamping;
     auto result = comm.reduce(
                           send_buf(local_top_k), //
@@ -76,15 +63,22 @@ TopK<K, ValueType> kamping_top_k(TopK<K, ValueType> const& local_top_k, kamping:
                               },
                               commutative))
                       .extract_recv_buffer();
-    std::cout << result[0];
-    return result[0];
+    if (comm.is_root()) {
+        return result[0];
+    } else {
+        return std::nullopt;
+    }
 }
 
 template <size_t K, typename ValueType>
-TopK<K, ValueType> mpi_top_k(TopK<K, ValueType> const& local_top_k, MPI_Comm comm) {
+std::optional<TopK<K, ValueType>> mpi_top_k(TopK<K, ValueType> const& local_top_k, MPI_Comm comm) {
+    // create a custom datatype
     MPI_Datatype topK_type;
+    // to make it truly generic we rely on KaMPIng
     MPI_Type_contiguous(K, kamping::mpi_datatype<ValueType>(), &topK_type);
     MPI_Type_commit(&topK_type);
+
+    // create a custom reduce operation
     MPI_Op             topK_merge_op;
     MPI_User_function* merge = [](void* invec, void* inoutvec, int* len, MPI_Datatype*) {
         TopK<K, ValueType>* invec_    = static_cast<TopK<K, ValueType>*>(invec);
@@ -107,12 +101,21 @@ TopK<K, ValueType> mpi_top_k(TopK<K, ValueType> const& local_top_k, MPI_Comm com
     };
     MPI_Op_create(merge, true, &topK_merge_op);
 
+    // the actual MPI call
     TopK<K, ValueType> global_top_k;
     MPI_Reduce(&local_top_k, &global_top_k, 1, topK_type, topK_merge_op, 0, comm);
 
+    // cleanup
     MPI_Op_free(&topK_merge_op);
     MPI_Type_free(&topK_type);
-    return global_top_k;
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    if (rank == 0) {
+        return global_top_k;
+    } else {
+        return std::nullopt;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -125,18 +128,17 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < K; ++i) {
         input[i] = comm.rank() + i * comm.size();
     }
-    std::cout << input << std::endl;
+    std::cout << "[R" << comm.rank() << "] local_input=" << input << std::endl;
 
     auto kamping_result = kamping_top_k(input, comm);
-    std::cout << kamping_result[0];
-    // if (comm.is_root()) {
-    //     std::cout << kamping_result << std::endl;
-    // }
+    if (comm.is_root()) {
+        std::cout << "global_result_kamping=" << kamping_result.value() << std::endl;
+    }
 
     auto mpi_result = mpi_top_k(input, MPI_COMM_WORLD);
     int  rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
-        std::cout << mpi_result << std::endl;
+        std::cout << "global_result_mpi=" << mpi_result.value() << std::endl;
     }
 }
