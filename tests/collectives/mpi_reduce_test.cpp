61b6eb0253b6faf7f843ee66c7bdd52b7989a98c
Lukas Hübner <lukashuebner@users.noreply.github.com>
2022-05-18 15:49:08 +0200

    Add check that the root of a gather, reduce or scatter call is the same on all ranks. (#303)
    Thanks Demian for the review :-)

--- tests/collectives/mpi_reduce_test.cpp
+++ tests/collectives/mpi_reduce_test.cpp
@@ -15,6 +15,7 @@
 
 #include "../helpers_for_testing.hpp"
 #include "kamping/collectives/reduce.hpp"
+#include "kamping/comm_helper/is_same_on_all_ranks.hpp"
 #include "kamping/communicator.hpp"
 
 using namespace ::kamping;
@@ -290,3 +291,15 @@ TEST(ReduceTest, reduce_custom_operation_on_custom_type) {
         EXPECT_EQ(result.size(), 0);
     }
 }
+
+// Death test do not work with MPI.
+// TEST(ReduceTest, reduce_different_roots_on_different_processes) {
+//     Communicator comm;
+//     auto         value = comm.rank();
+//
+//     if (kassert::internal::assertion_enabled(assert::light_communication) && comm.size() > 1) {
+//         EXPECT_KASSERT_FAILS(
+//             comm.reduce(send_buf(value), op(kamping::ops::plus<>{}), root(comm.rank())),
+//             "Root has to be the same on all ranks.");
+//     }
+// }
