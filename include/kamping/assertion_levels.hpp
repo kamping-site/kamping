2568b16ee5513a887498c6e03b6d9fe629080f8b
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-06-13 14:20:47 +0200

    Levels

--- include/kamping/assertion_levels.hpp
+++ include/kamping/assertion_levels.hpp
@@ -12,29 +12,29 @@ namespace kamping::assert {
 /// @brief Assertion level for lightweight assertions.
 constexpr int light = KAMPING_ASSERTION_LEVEL_LIGHT;
 
-/// @brief Default assertion level. This level is used if no assertion level is specified.
-#define KAMPING_ASSERTION_LEVEL_NORMAL 30
-
-/// @brief Default assertion level. This level is used if no assertion level is specified.
-constexpr int normal = KAMPING_ASSERTION_LEVEL_NORMAL;
-
 /// @brief Assertions that perform lightweight communication.
-#define KAMPING_ASSERTION_LEVEL_LIGHT_COMMUNICATION 40
+#define KAMPING_ASSERTION_LEVEL_LIGHT_COMMUNICATION 30
 
 /// @brief Assertions that perform lightweight communication.
 constexpr int light_communication = KAMPING_ASSERTION_LEVEL_LIGHT_COMMUNICATION;
 
-/// @brief Assertions that perform heavyweight communication.
-#define KAMPING_ASSERTION_LEVEL_HEAVY_COMMUNICATION 50
+/// @brief Default assertion level. This level is used if no assertion level is specified.
+#define KAMPING_ASSERTION_LEVEL_NORMAL 40
 
-/// @brief Assertions that perform heavyweight communication.
-constexpr int heavy_communication = KAMPING_ASSERTION_LEVEL_HEAVY_COMMUNICATION;
+/// @brief Default assertion level. This level is used if no assertion level is specified.
+constexpr int normal = KAMPING_ASSERTION_LEVEL_NORMAL;
 
 /// @brief Assertion level for heavyweight assertions.
-#define KAMPING_ASSERTION_LEVEL_HEAVY 60
+#define KAMPING_ASSERTION_LEVEL_HEAVY 50
 
 /// @brief Assertion level for heavyweight assertions.
 constexpr int heavy = KAMPING_ASSERTION_LEVEL_HEAVY;
+
+/// @brief Assertions that perform heavyweight communication.
+#define KAMPING_ASSERTION_LEVEL_HEAVY_COMMUNICATION 60
+
+/// @brief Assertions that perform heavyweight communication.
+constexpr int heavy_communication = KAMPING_ASSERTION_LEVEL_HEAVY_COMMUNICATION;
 
 /// @}
 } // namespace kamping::assert
