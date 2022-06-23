a91ccba6409bfa8ba37464e7f07ba7dcf5c73e23
Daniel Seemaier <17594350+DanielSeemaier@users.noreply.github.com>
2022-06-23 15:15:54 +0200

    Improve testability of input validation (#329)
    * Implement testing header to turn assertions into exceptions
    
    * Remove old comment
    
    * Improve documentation
    
    * WIP
    
    * Update kassert
    
    * Allow names for assertion levels
    
    * Levels
    
    * Add comment
    
    * Documentation, move header to correct position by clang-format
    
    * Change level order
    
    * Update tests/test_assertions.hpp
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>
    
    * Update tests/test_assertions.hpp
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>
    
    * Add EXCEPT_KASSERT_FAILS to assertion-exception-replacement
    
    * Format
    
    * Only test assertions if we build with heavy assertion level
    
    * Test with heavy assertions
    
    * Use new checks in more tests
    
    * typo
    
    * typo
    
    * undo
    
    * .
    
    * .
    
    * Add KAMPING_NOEXCEPT macro
    
    * Add KAMPING_NOEXCEPT macro
    
    * Remove unused if
    
    * Doc
    
    * Update include/kamping/noexcept.hpp
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>
    
    * Update include/kamping/noexcept.hpp
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>
    
    * Add doc
    
    * Add doc
    
    * Fix level
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>

--- tests/collectives/mpi_scatter_test.cpp
+++ tests/collectives/mpi_scatter_test.cpp
@@ -11,6 +11,8 @@
 // You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
 // <https://www.gnu.org/licenses/>.
 
+#include "../test_assertions.hpp"
+
 #include <numeric>
 #include <vector>
 
@@ -191,13 +193,10 @@ TEST(ScatterTest, scatter_with_nonempty_sendbuf_on_non_root) {
     EXPECT_EQ(result.front(), comm.rank());
 }
 
-// Death test do not work with MPI.
-// TEST(ScatterTest, scatter_different_roots_on_different_processes) {
-//     Communicator comm;
-//     auto const   input = create_input_vector_on_root(comm, 1);
-//
-//     if (kassert::internal::assertion_enabled(assert::light_communication) && comm.size() > 1) {
-//         EXPECT_KASSERT_FAILS(comm.scatter(send_buf(input), root(comm.rank())), "Root has to be the same on all
-//         ranks.");
-//     }
-// }
+TEST(ScatterTest, scatter_different_roots_on_different_processes) {
+    Communicator comm;
+    auto const   input = create_input_vector_on_root(comm, 1);
+    if (comm.size() > 1) {
+        EXPECT_KASSERT_FAILS(comm.scatter(send_buf(input), root(comm.rank())), "");
+    }
+}
