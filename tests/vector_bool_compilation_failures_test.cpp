a2fcf3dcc5e276dccf6e693d0d5a1f57d115acc6
Tim Niklas Uhl <uhl@kit.edu>
2022-07-06 16:29:34 +0200

    comments for compilation failure tests

--- tests/vector_bool_compilation_failures_test.cpp
+++ tests/vector_bool_compilation_failures_test.cpp
@@ -11,28 +11,34 @@
 // You should have received a copy of the GNU Lesser General Public License along with KaMPI.ng.  If not, see
 // <https://www.gnu.org/licenses/>.
 
+#include <functional>
+
 #include "helpers_for_testing.hpp"
+#include "kamping/collectives/gather.hpp"
 #include "kamping/communicator.hpp"
 #include "kamping/parameter_factories.hpp"
 
 int main(int /*argc*/, char** /*argv*/) {
     using namespace kamping;
     bool                single_element_bool [[maybe_unused]]   = false;
-    bool                single_element_kabool [[maybe_unused]] = false;
+    kabool              single_element_kabool [[maybe_unused]] = false;
     std::vector<bool>   vector_bool                            = {false, true, false};
     std::vector<kabool> vector_kabool                          = {false, true, false};
     Communicator        comm;
 
 #ifdef SINGLE_BOOL_VEC_BOOL
-    comm.gather(send_buf(single_element_bool), recv_buf(vector_bool));
+    // the recv_buf would be a std::vector<bool>, which is not supported
+    // solution: receive into a buffer which is not std::vector or use kabool
+    comm.gather(send_buf(single_element_bool));
 #elif SINGLE_KABOOL_VEC_BOOL
+    // the recv_buf is a std::vector<bool>, which is not supported
     comm.gather(send_buf(single_element_kabool), recv_buf(vector_bool));
 #elif SEND_VEC_BOOL
+    // the send_buf is a std::vector<bool>, which is not supported
     comm.gather(send_buf(vector_bool));
 #elif SEND_VEC_KABOOL_RECV_VEC_BOOL
+    // the recv_buf is a std::vector<bool>, which is not supported
     comm.gather(send_buf(vector_kabool), recv_buf(vector_bool));
-#elif SEND_BOOL_INITIALIZER_LIST
-    comm.gather(send_buf({kabool{true}, kabool{false}, kabool{true}}), recv_buf(vector_kabool));
 // If none of the above sections is active, this file will compile successfully.
 #endif
 }
