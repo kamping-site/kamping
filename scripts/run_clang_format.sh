af2c7937212c9428d97e7f8c77916b49d0af7407
Niklas Uhl <niklas.uhl@online.de>
2022-07-08 13:47:20 +0200

    Revert "Soft-force clang-format-13 (#362)"
    This reverts commit ef977654b4b904ddfb3b29a3556120596821a0cd.

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -5,14 +5,9 @@ if [[ "$PWD" == */scripts ]]; then
     exit 1
 fi
 
-# Get the absolute location of this script 
-# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
-SOURCE=${BASH_SOURCE[0]}
-
-formatter=$("$SOURCE/get_clang_format.sh")
 for directory in "include" "tests" "examples"; do
     find "$directory"                           \
         -type f                                 \
         \( -name "*.cpp" -or -name "*.hpp" \)   \
-        -exec $formatter -i {} \;
+        -exec clang-format -i {} \;
 done
