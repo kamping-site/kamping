554204b37eff776d4ee3555cab34bc5f25ecfc0c
Niklas Uhl <tim.uhl@kit.edu>
2022-07-08 14:14:35 +0200

    Now copy the whole code for getting the script path (#365)

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -8,8 +8,14 @@ fi
 # Get the absolute location of this script 
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
-formatter=$("$SOURCE/get_clang_format.sh")
+formatter=$("${SCRIPT_DIR}/get_clang_format.sh")
 for directory in "include" "tests" "examples"; do
     find "$directory"                           \
         -type f                                 \
