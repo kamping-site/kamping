8964d357b4d22cf82e1acbb0c464ae4b8986f9cd
Tim Niklas Uhl <uhl@kit.edu>
2022-07-08 11:21:43 +0200

    Use

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -5,9 +5,19 @@ if [[ "$PWD" == */scripts ]]; then
     exit 1
 fi
 
+clang_format_version=13
+
+# Use clang-format-<version> if available, else use clang-format
+if command -v clang-format-$clang_format_version &> /dev/null
+then
+    formatter=clang-format-$clang_format_version
+else
+    formatter=clang-format
+fi
+
 for directory in "include" "tests" "examples"; do
     find "$directory"                           \
         -type f                                 \
         \( -name "*.cpp" -or -name "*.hpp" \)   \
-        -exec clang-format -i {} \;
+        -exec $formatter -i {} \;
 done
