e4447186b03c22df06912601db9fbe08f6e047da
Lukas Hübner <lukas@klhuebner.de>
2022-08-11 16:31:11 +0200

    Unify upper/lower-case of variable names.

--- scripts/get_cmake_format.sh
+++ scripts/get_cmake_format.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CMAKE_FORMAT="cmake-format"
+cmake_format="cmake-format"
 
 EXIT_SUCCESS=0
 EXIT_EXEC_NOT_FOUND=1
 
 # cmake-format does not have multiple versions yet, simply return cmake-format
-if command -v "$CMAKE_FORMAT" >/dev/null 2>&1; then
-    command -v "$CMAKE_FORMAT"
+if command -v "$cmake_format" >/dev/null 2>&1; then
+    command -v "$cmake_format"
     exit $EXIT_SUCCESS
 else
     exit $EXIT_EXEC_NOT_FOUND
