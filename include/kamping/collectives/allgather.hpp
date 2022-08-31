d95652e483f6d9d702d9b051c7cd4715b571b606
Lukas Hübner <lukas@klhuebner.de>
2022-08-31 11:57:57 +0200

    Add allgather()

--- include/kamping/collectives/allgather.hpp
+++ include/kamping/collectives/allgather.hpp
@@ -28,15 +28,14 @@
 #include "kamping/named_parameter_types.hpp"
 #include "kamping/named_parameters.hpp"
 
-/// @brief Wrapper for \c MPI_Gather.
+/// @brief Wrapper for \c MPI_Allgather.
 ///
-/// This wrapper for \c MPI_Gather collects the same amount of data from each rank to a root.
+/// This wrapper for \c MPI_Allgather collects the same amount of data from each rank to all ranks. It is semantically
+/// equivalent to performing a \c gather() followed by a broadcast of the collected data.
 ///
 /// The following arguments are required:
 /// - \ref kamping::send_buf() containing the data that is sent to the root. This buffer has to be the same size at
 /// each rank. See TODO gather_v if the amounts differ. The following buffers are optional:
-/// - \ref kamping::root() specifying an alternative root. If not present, the default root of the \c Communicator
-/// is used, see root().
 /// - \ref kamping::recv_buf() containing a buffer for the output. Afterwards, at the root, this buffer will contain
 /// all data from all send buffers. At all other ranks, the buffer will have size 0.
 ///
@@ -44,16 +43,16 @@
 /// @param args All required and any number of the optional buffers described above.
 /// @return Result type wrapping the output buffer if not specified as input parameter.
 template <typename... Args>
-auto kamping::Communicator::gather(Args... args) const {
+auto kamping::Communicator::allgather(Args... args) const {
     using namespace kamping::internal;
-    KAMPING_CHECK_PARAMETERS(Args, KAMPING_REQUIRED_PARAMETERS(send_buf), KAMPING_OPTIONAL_PARAMETERS(recv_buf, root));
+    KAMPING_CHECK_PARAMETERS(Args, KAMPING_REQUIRED_PARAMETERS(send_buf), KAMPING_OPTIONAL_PARAMETERS(recv_buf));
 
     auto& send_buf_param  = internal::select_parameter_type<internal::ParameterType::send_buf>(args...);
     auto  send_buf        = send_buf_param.get();
     using send_value_type = typename std::remove_reference_t<decltype(send_buf_param)>::value_type;
     KASSERT(
         is_same_on_all_ranks(send_buf.size()),
-        "All PEs have to send the same number of elements. Use gatherv, if you want to send a different number of "
+        "All PEs have to send the same number of elements. Use allgatherv, if you want to send a different number of "
         "elements.",
         assert::light_communication
     );
@@ -67,37 +66,25 @@ auto kamping::Communicator::gather(Args... args) const {
         );
     using recv_value_type = typename std::remove_reference_t<decltype(recv_buf)>::value_type;
 
-    auto&& root = internal::select_parameter_type_or_default<internal::ParameterType::root, internal::RootDataBuffer>(
-        std::tuple(this->root()),
-        args...
-    );
-    KASSERT(this->is_valid_rank(root.rank()), "Invalid rank as root.");
-    KASSERT(
-        this->is_same_on_all_ranks(root.rank()),
-        "Root has to be the same on all ranks.",
-        assert::light_communication
-    );
-
     auto mpi_send_type = mpi_datatype<send_value_type>();
     auto mpi_recv_type = mpi_datatype<recv_value_type>();
     KASSERT(mpi_send_type == mpi_recv_type, "The specified receive type does not match the send type.");
 
-    size_t recv_size     = (this->rank() == root.rank()) ? send_buf.size() : 0;
+    size_t recv_size     = send_buf.size();
     size_t recv_buf_size = this->size() * recv_size;
 
     // error code can be unused if KTHROW is removed at compile time
     recv_buf.resize(recv_buf_size);
-    [[maybe_unused]] int err = MPI_Gather(
+    [[maybe_unused]] int err = MPI_Allgather(
         send_buf.data(),
         asserting_cast<int>(send_buf.size()),
         mpi_send_type,
         recv_buf.data(),
         asserting_cast<int>(recv_size),
         mpi_recv_type,
-        root.rank_signed(),
         this->mpi_communicator()
     );
-    THROW_IF_MPI_ERROR(err, MPI_Gather);
+    THROW_IF_MPI_ERROR(err, MPI_Allgather);
     return MPIResult(
         std::move(recv_buf),
         internal::BufferCategoryNotUsed{},
---
+++ tests/collectives/mpi_allgather_test.cpp
@@ -0,0 +1,230 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2021 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include "gmock/gmock.h"
+#include <cstddef>
+
+#include <gtest/gtest.h>
+#include <mpi.h>
+
+#include "../helpers_for_testing.hpp"
+#include "kamping/collectives/allgather.hpp"
+#include "kamping/comm_helper/is_same_on_all_ranks.hpp"
+#include "kamping/communicator.hpp"
+#include "kamping/mpi_datatype.hpp"
+#include "kamping/named_parameters.hpp"
+
+using namespace ::kamping;
+using namespace ::testing;
+
+TEST(AllgatherTest, allgather_single_element_no_receive_buffer) {
+    Communicator comm;
+    auto         value = comm.rank();
+
+    auto result = comm.allgather(send_buf(value)).extract_recv_buffer();
+    EXPECT_EQ(comm.root(), 0);
+    ASSERT_EQ(result.size(), comm.size());
+    for (size_t i = 0; i < comm.size(); ++i) {
+        EXPECT_EQ(result[i], i);
+    }
+
+    // Change default root and test with communicator's default root again, this should not change anything.
+    comm.root(comm.size_signed() - 1);
+    result = comm.allgather(send_buf(value)).extract_recv_buffer();
+    EXPECT_EQ(comm.root(), comm.size() - 1);
+    ASSERT_EQ(result.size(), comm.size());
+    for (size_t i = 0; i < comm.size(); ++i) {
+        EXPECT_EQ(result[i], i);
+    }
+}
+
+TEST(AllgatherTest, allgather_single_custom_element_no_receive_buffer) {
+    Communicator comm;
+    struct CustomDataType {
+        int rank;
+        int additional_value;
+    }; // struct custom_data_type
+
+    CustomDataType value = {comm.rank_signed(), comm.size_signed() - comm.rank_signed()};
+
+    auto result = comm.allgather(send_buf(value)).extract_recv_buffer();
+    ASSERT_EQ(result.size(), comm.size());
+    for (size_t i = 0; i < comm.size(); ++i) {
+        EXPECT_EQ(result[i].rank, i);
+        EXPECT_EQ(result[i].additional_value, comm.size() - i);
+    }
+}
+
+TEST(AllgatherTest, allgather_single_element_with_receive_buffer) {
+    Communicator                 comm;
+    auto                         value = comm.rank();
+    std::vector<decltype(value)> result(0);
+
+    comm.allgather(send_buf(value), recv_buf(result));
+    ASSERT_EQ(result.size(), comm.size());
+    for (size_t i = 0; i < comm.size(); ++i) {
+        EXPECT_EQ(result[i], i);
+    }
+}
+
+TEST(AllgatherTest, allgather_multiple_elements_no_receive_buffer) {
+    Communicator     comm;
+    std::vector<int> values = {comm.rank_signed(), comm.rank_signed(), comm.rank_signed(), comm.rank_signed()};
+    auto             result = comm.allgather(send_buf(values)).extract_recv_buffer();
+
+    EXPECT_EQ(result.size(), values.size() * comm.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        EXPECT_EQ(result[i], i / values.size());
+    }
+}
+
+TEST(AllgatherTest, allgather_multiple_elements_with_receive_buffer) {
+    Communicator     comm;
+    std::vector<int> values = {comm.rank_signed(), comm.rank_signed(), comm.rank_signed(), comm.rank_signed()};
+    std::vector<int> result(0);
+
+    comm.allgather(send_buf(values), recv_buf(result));
+
+    EXPECT_EQ(result.size(), values.size() * comm.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        EXPECT_EQ(result[i], i / values.size());
+    }
+}
+
+TEST(AllgatherTest, allgather_receive_custom_container) {
+    Communicator      comm;
+    std::vector<int>  values = {comm.rank_signed(), comm.rank_signed(), comm.rank_signed(), comm.rank_signed()};
+    OwnContainer<int> result;
+
+    comm.allgather(send_buf(values), recv_buf(result));
+
+    EXPECT_EQ(result.size(), values.size() * comm.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        EXPECT_EQ(result[i], i / values.size());
+    }
+}
+
+TEST(AllgatherTest, allgather_send_custom_container) {
+    Communicator      comm;
+    OwnContainer<int> values(4);
+    for (size_t i = 0; i < values.size(); ++i) {
+        values[i] = comm.rank_signed();
+    }
+    std::vector<int> result;
+
+    comm.allgather(send_buf(values), recv_buf(result));
+
+    EXPECT_EQ(result.size(), values.size() * comm.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        EXPECT_EQ(result[i], i / values.size());
+    }
+}
+
+TEST(AllgatherTest, allgather_send_and_receive_custom_container) {
+    Communicator      comm;
+    OwnContainer<int> values(4);
+    for (size_t i = 0; i < values.size(); ++i) {
+        values[i] = comm.rank_signed();
+    }
+    OwnContainer<int> result;
+
+    comm.allgather(send_buf(values), recv_buf(result));
+
+    EXPECT_EQ(result.size(), values.size() * comm.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        EXPECT_EQ(result[i], i / values.size());
+    }
+}
+
+TEST(AllgatherTest, allgather_single_element_initializer_list_bool_no_receive_buffer) {
+    Communicator comm;
+    // gather does not support single element bool when specifying no recv_buffer, because the default receive buffer is
+    // std::vector<bool>, which is not supported
+    auto result = comm.allgather(send_buf({false})).extract_recv_buffer();
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), comm.size());
+    for (auto elem: result) {
+        EXPECT_EQ(elem, false);
+    }
+}
+
+TEST(AllgatherTest, allgather_initializer_list_bool_no_receive_buffer) {
+    Communicator comm;
+    auto         result = comm.allgather(send_buf({false, false})).extract_recv_buffer();
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), 2 * comm.size());
+    for (auto elem: result) {
+        EXPECT_EQ(elem, false);
+    }
+}
+
+TEST(AllgatherTest, allgather_single_element_kabool_no_receive_buffer) {
+    Communicator comm;
+    auto         result = comm.allgather(send_buf(kabool{false})).extract_recv_buffer();
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), comm.size());
+    for (auto elem: result) {
+        EXPECT_EQ(elem, false);
+    }
+}
+
+TEST(AllgatherTest, allgather_single_element_bool_with_receive_buffer) {
+    Communicator        comm;
+    std::vector<kabool> result;
+    comm.allgather(send_buf({false}), recv_buf(result));
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), comm.size());
+    for (auto elem: result) {
+        EXPECT_EQ(elem, false);
+    }
+}
+
+TEST(AllgatherTest, allgather_single_element_kabool_with_receive_buffer) {
+    Communicator        comm;
+    std::vector<kabool> result;
+    comm.allgather(send_buf(kabool{false}), recv_buf(result));
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), comm.size());
+    for (auto elem: result) {
+        EXPECT_EQ(elem, false);
+    }
+}
+
+TEST(AllgatherTest, allgather_multiple_elements_kabool_no_receive_buffer) {
+    Communicator        comm;
+    std::vector<kabool> input  = {false, true};
+    auto                result = comm.allgather(send_buf(input)).extract_recv_buffer();
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), 2 * comm.size());
+    for (size_t i = 0; i < result.size(); i++) {
+        EXPECT_EQ((i % 2 != 0), result[i]);
+    }
+}
+
+TEST(AllgatherTest, allgather_multiple_elements_kabool_with_receive_buffer) {
+    Communicator        comm;
+    std::vector<kabool> input = {false, true};
+    std::vector<kabool> result;
+    comm.allgather(send_buf(input), recv_buf(result));
+
+    KASSERT((std::is_same_v<decltype(result), std::vector<kabool>>));
+    EXPECT_EQ(result.size(), 2 * comm.size());
+    for (size_t i = 0; i < result.size(); i++) {
+        EXPECT_EQ((i % 2 != 0), result[i]);
+    }
+}
