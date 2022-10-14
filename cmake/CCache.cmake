b4c0ce038337223adba95b9ad95aca62a94c48f1
Tim Niklas Uhl <uhl@kit.edu>
2022-10-14 09:15:07 +0200

    Remove unnecessary code

--- cmake/CCache.cmake
+++ cmake/CCache.cmake
@@ -1,7 +1,3 @@
-# Adopted from: https://crascit.com/2016/04/09/using-ccache-with-cmake/#more-410
-
-set(CCACHE_LAUNCHER_DIR "${CMAKE_CURRENT_LIST_DIR}")
-
 find_program(CCACHE_PROGRAM ccache)
 if (CCACHE_PROGRAM)
     message(STATUS "Using ccache to speed up compilation.")
@@ -12,6 +8,7 @@ if (CCACHE_PROGRAM)
         set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
     else ()
         message(WARNING "${CMAKE_GENERATOR} does not support <LANG>_COMPILER_LAUNCHER.")
+        # See https://crascit.com/2016/04/09/using-ccache-with-cmake/#more-410 if you absolutely want to use XCode
     endif ()
 else ()
     message(WARNING "Could not find ccache in your PATH.")
