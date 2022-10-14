018e2963190dc5e9d22e84657177d2966b0435a4
Lukas Hübner <lukashuebner@users.noreply.github.com>
2022-10-14 10:51:32 +0200

    Speed up compilation with ccache if present. (#407)
    * Speed up compilation with ccache if present.
    
    * Use CMake-style naming
    
    * Cleanup
    
    * Use option instead of set
    
    * Update CMakeLists.txt
    
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>
    
    * Better comment
    
    * Remove unnecessary code
    
    Co-authored-by: Tim Niklas Uhl <uhl@kit.edu>
    Co-authored-by: Niklas Uhl <tim.uhl@kit.edu>
    Co-authored-by: Demian Hespe <Hespian@users.noreply.github.com>

---
+++ cmake/CCache.cmake
@@ -0,0 +1,15 @@
+find_program(CCACHE_PROGRAM ccache)
+if (CCACHE_PROGRAM)
+    message(STATUS "Using ccache to speed up compilation.")
+
+    set(SUPPORTED_GENERATORS "Unix Makefiles" "Ninja")
+    if (CMAKE_GENERATOR IN_LIST SUPPORTED_GENERATORS)
+        set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+        set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+    else ()
+        message(WARNING "${CMAKE_GENERATOR} does not support <LANG>_COMPILER_LAUNCHER.")
+        # See https://crascit.com/2016/04/09/using-ccache-with-cmake/#more-410 if you absolutely want to use XCode
+    endif ()
+else ()
+    message(WARNING "Could not find ccache in your PATH.")
+endif ()
