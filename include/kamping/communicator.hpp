0b818a1d340f800ca77d7870b5db5b161da0614a
Demian Hespe <hespe@kit.edu>
2022-12-06 13:03:37 +0100

    Add example plugin with a data member

--- include/kamping/communicator.hpp
+++ include/kamping/communicator.hpp
@@ -29,7 +29,7 @@ namespace kamping {
 /// @tparam DefaultContainerType The default container type to use for containers created by KaMPIng. Defaults to
 /// std::vector.
 /// @tparam Plugins Plugins adding functionality to KaMPIng. Plugins should be classes taking a ``Communicator``
-/// template parameter, have no data members and can assume that they are castable to `Communicator` from which they can
+/// template parameter and can assume that they are castable to `Communicator` from which they can
 /// call any function of `kamping::Communicator`. See `test/plugin_tests.cpp` for examples.
 template <template <typename...> typename DefaultContainerType = std::vector, template <typename> typename... Plugins>
 class Communicator : public Plugins<Communicator<DefaultContainerType, Plugins...>>... {
