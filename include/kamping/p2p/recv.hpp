60ccf37f57565e29c7d97138f72467461f41993b
Niklas Uhl <tim.uhl@kit.edu>
2022-12-12 13:49:14 +0100

    Implement receive (#455)

---
+++ include/kamping/p2p/recv.hpp
@@ -0,0 +1,141 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2022 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version. KaMPIng is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with KaMPIng.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+#include <kassert/kassert.hpp>
+#include <mpi.h>
+
+#include "kamping/communicator.hpp"
+#include "kamping/data_buffer.hpp"
+#include "kamping/error_handling.hpp"
+#include "kamping/implementation_helpers.hpp"
+#include "kamping/mpi_datatype.hpp"
+#include "kamping/mpi_function_wrapper_helpers.hpp"
+#include "kamping/named_parameter_check.hpp"
+#include "kamping/named_parameter_selection.hpp"
+#include "kamping/named_parameter_types.hpp"
+#include "kamping/named_parameters.hpp"
+#include "kamping/parameter_objects.hpp"
+#include "kamping/status.hpp"
+
+/// @brief Wrapper for \c MPI_Recv.
+///
+/// This wraps \c MPI_Recv. This operation performs a standard blocking receive.
+/// If the \ref kamping::send_counts() parameter is not specified, this first performs a probe, followed by a receive of
+/// the probed message with the probed message size.
+///
+/// The following parameter is required:
+/// - \ref kamping::recv_buf() the buffer to receive the message into. If possible, this buffer will be resized to
+/// accommodate the number of elements to receive. Use \c kamping::Span with enough space if you do not want the buffer
+/// to be resized.
+///
+/// The following parameters are optional:
+/// - \ref kamping::tag() recv message with this tag. Defaults to receiving
+/// for an arbitrary tag, i.e. \c tag(tags::any).
+/// - \ref kamping::source() receive a message sent from this source rank.
+/// Defaults to probing for an arbitrary source, i.e. \c source(rank::any).
+/// - \ref kamping::status() or \ref kamping::status_out(). Returns info about
+/// the received message by setting the appropriate fields in the status object
+/// passed by the user. If \ref kamping::status_out() is passed, constructs a
+/// status object which may be retrieved by the user. The status can be ignored by
+/// passing \c kamping::status(kamping::ignore<>). This is the default.
+///
+/// The following parameter is optional, but leads to an additional call to \c MPI_Probe if not present:
+/// - \ref kamping::send_counts() the number of elements to receive. Will be probed before receiving if not given.
+///
+/// @tparam Args Automatically deducted template parameters.
+/// @param args All required and any number of the optional buffers described
+/// above.
+template <template <typename...> typename DefaultContainerType, template <typename> typename... Plugins>
+template <typename... Args>
+auto kamping::Communicator<DefaultContainerType, Plugins...>::recv(Args... args) const {
+    KAMPING_CHECK_PARAMETERS(
+        Args,
+        KAMPING_REQUIRED_PARAMETERS(recv_buf),
+        KAMPING_OPTIONAL_PARAMETERS(tag, source, recv_counts, status)
+    );
+
+    auto& recv_buf        = internal::select_parameter_type<internal::ParameterType::recv_buf>(args...);
+    using recv_value_type = typename std::remove_reference_t<decltype(recv_buf)>::value_type;
+
+    using default_source_buf_type = decltype(kamping::source(rank::any));
+
+    auto&& source_param =
+        internal::select_parameter_type_or_default<internal::ParameterType::source, default_source_buf_type>(
+            {},
+            args...
+        );
+
+    using default_tag_buf_type = decltype(kamping::tag(tags::any));
+
+    auto&& tag_param =
+        internal::select_parameter_type_or_default<internal::ParameterType::tag, default_tag_buf_type>({}, args...);
+
+    using default_status_param_type = decltype(kamping::status(kamping::ignore<>));
+
+    auto&& status =
+        internal::select_parameter_type_or_default<internal::ParameterType::status, default_status_param_type>(
+            {},
+            args...
+        );
+
+    // Get the optional recv_count parameter. If the parameter is not given,
+    // allocate a new container.
+    using default_recv_count_type = decltype(kamping::recv_counts_out(NewContainer<int>{}));
+    auto&& recv_count_param =
+        internal::select_parameter_type_or_default<internal::ParameterType::recv_counts, default_recv_count_type>(
+            std::tuple(),
+            args...
+        );
+
+    KASSERT(internal::is_valid_rank_in_comm(source_param, *this, true, true));
+    int            source                         = source_param.rank_signed();
+    int            tag                            = tag_param.tag();
+    constexpr bool recv_count_is_output_parameter = internal::has_to_be_computed<decltype(recv_count_param)>;
+    static_assert(
+        std::remove_reference_t<decltype(recv_count_param)>::is_single_element,
+        "recv_counts() parameter must be a single value."
+    );
+    if constexpr (recv_count_is_output_parameter) {
+        Status probe_status;
+        MPI_Probe(source, tag, this->mpi_communicator(), &probe_status.native());
+        source                   = probe_status.source_signed();
+        tag                      = probe_status.tag();
+        *recv_count_param.data() = asserting_cast<int>(probe_status.template count<recv_value_type>());
+    }
+
+    // Ensure that we do not touch the recv buffer if MPI_PROC_NULL is passed,
+    // because this is what the standard guarantees.
+    if constexpr (std::remove_reference_t<decltype(source_param)>::rank_type != internal::RankType::null) {
+        recv_buf.resize(asserting_cast<size_t>(recv_count_param.get_single_element()));
+    }
+
+    [[maybe_unused]] int err = MPI_Recv(
+        recv_buf.data(),                                            // buf
+        asserting_cast<int>(recv_count_param.get_single_element()), // count
+        mpi_datatype<recv_value_type>(),                            // dataype
+        source,                                                     // source
+        tag,                                                        // tag
+        this->mpi_communicator(),                                   // comm
+        status.native_ptr()                                         // status
+    );
+    THROW_IF_MPI_ERROR(err, MPI_Recv);
+
+    return make_mpi_result(std::move(recv_buf), std::move(recv_count_param), std::move(status));
+}
---
+++ tests/p2p/mpi_recv_test.cpp
@@ -0,0 +1,240 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2022 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version. KaMPIng is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with KaMPIng.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include <mpi.h>
+
+#include "kamping/checking_casts.hpp"
+#include "kamping/communicator.hpp"
+#include "kamping/has_member.hpp"
+#include "kamping/named_parameters.hpp"
+#include "kamping/p2p/recv.hpp"
+
+using namespace kamping;
+
+KAMPING_MAKE_HAS_MEMBER(extract_recv_counts)
+KAMPING_MAKE_HAS_MEMBER(extract_status)
+
+static size_t call_hierarchy_level = 0;
+static size_t probe_counter        = 0;
+
+// call-counting wrapper for probe
+int MPI_Probe(int source, int tag, MPI_Comm comm, MPI_Status* status) {
+    call_hierarchy_level++;
+    auto errcode = PMPI_Probe(source, tag, comm, status);
+    // a probe call may call another operation in its implementation
+    // this ensures that we only count the top level probe operation
+    if (call_hierarchy_level == 1) {
+        probe_counter++;
+    }
+    call_hierarchy_level--;
+    return errcode;
+}
+
+class RecvTest : public ::testing::Test {
+    void SetUp() override {
+        call_hierarchy_level = 0;
+        probe_counter        = 0;
+    }
+    void TearDown() override {
+        call_hierarchy_level = 0;
+        probe_counter        = 0;
+    }
+};
+
+TEST_F(RecvTest, recv_vector_from_arbitrary_source) {
+    Communicator     comm;
+    std::vector<int> v(comm.rank(), 42);
+    MPI_Request      req;
+    // Each rank sends a message with its rank as tag to rank 0.
+    // The message has comm.rank() elements.
+    MPI_Issend(
+        v.data(),                      // send_buf
+        asserting_cast<int>(v.size()), // send_count
+        MPI_INT,                       // send_type
+        0,                             // destination
+        comm.rank_signed(),            // tag
+        comm.mpi_communicator(),       // comm
+        &req                           // request
+    );
+    if (comm.rank() == 0) {
+        for (size_t other = 0; other < comm.size(); other++) {
+            std::vector<int> message;
+            auto             result = comm.recv(recv_buf(message), status_out());
+            EXPECT_TRUE(has_member_extract_recv_counts_v<decltype(result)>);
+            EXPECT_TRUE(has_member_extract_status_v<decltype(result)>);
+            auto status = result.extract_status();
+            auto source = status.source();
+            EXPECT_EQ(status.tag(), source);
+            EXPECT_EQ(status.count<int>(), source);
+            EXPECT_EQ(message.size(), source);
+            EXPECT_EQ(result.extract_recv_counts(), source);
+            EXPECT_EQ(message, std::vector(source, 42));
+        }
+    }
+    // ensure that we have received all inflight messages
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+}
+
+TEST_F(RecvTest, recv_vector_from_explicit_source) {
+    Communicator     comm;
+    std::vector<int> v(comm.rank(), 42);
+    MPI_Request      req;
+    // Each rank sends a message with its rank as tag to rank 0.
+    // The message has comm.rank() elements.
+    MPI_Issend(
+        v.data(),                      // send_buf
+        asserting_cast<int>(v.size()), // send_count
+        MPI_INT,                       // send_type
+        0,                             // destination
+        comm.rank_signed(),            // tag
+        comm.mpi_communicator(),       // comm
+        &req                           // request
+    );
+    if (comm.rank() == 0) {
+        for (size_t other = 0; other < comm.size(); other++) {
+            std::vector<int> message;
+            auto             result = comm.recv(source(other), recv_buf(message), status_out());
+            EXPECT_TRUE(has_member_extract_recv_counts_v<decltype(result)>);
+            EXPECT_TRUE(has_member_extract_status_v<decltype(result)>);
+            auto status = result.extract_status();
+            auto source = status.source();
+            EXPECT_EQ(source, other);
+            EXPECT_EQ(status.tag(), source);
+            EXPECT_EQ(status.count<int>(), source);
+            EXPECT_EQ(message.size(), source);
+            EXPECT_EQ(result.extract_recv_counts(), source);
+            EXPECT_EQ(message, std::vector(source, 42));
+        }
+    }
+    // ensure that we have received all inflight messages
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+}
+
+TEST_F(RecvTest, recv_vector_from_explicit_source_and_explicit_tag) {
+    Communicator     comm;
+    std::vector<int> v(comm.rank(), 42);
+    MPI_Request      req;
+    // Each rank sends a message with its rank as tag to rank 0.
+    // The message has comm.rank() elements.
+    MPI_Issend(
+        v.data(),                      // send_buf
+        asserting_cast<int>(v.size()), // send_count
+        MPI_INT,                       // send_type
+        0,                             // destination
+        comm.rank_signed(),            // tag
+        comm.mpi_communicator(),       // comm
+        &req                           // request
+    );
+    if (comm.rank() == 0) {
+        for (size_t other = 0; other < comm.size(); other++) {
+            std::vector<int> message;
+            auto result = comm.recv(source(other), tag(asserting_cast<int>(other)), recv_buf(message), status_out());
+            EXPECT_TRUE(has_member_extract_recv_counts_v<decltype(result)>);
+            EXPECT_TRUE(has_member_extract_status_v<decltype(result)>);
+            auto status = result.extract_status();
+            auto source = status.source();
+            EXPECT_EQ(source, other);
+            EXPECT_EQ(status.tag(), source);
+            EXPECT_EQ(status.count<int>(), source);
+            EXPECT_EQ(message.size(), source);
+            EXPECT_EQ(result.extract_recv_counts(), source);
+            EXPECT_EQ(message, std::vector(source, 42));
+        }
+    }
+    // ensure that we have received all inflight messages
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+}
+
+TEST_F(RecvTest, recv_vector_with_explicit_size) {
+    Communicator comm;
+    std::vector  v{1, 2, 3, 4, 5};
+    MPI_Request  req = MPI_REQUEST_NULL;
+    if (comm.is_root()) {
+        auto other_rank = comm.rank_shifted_cyclic(1);
+        MPI_Isend(
+            v.data(),                        // send_buf
+            asserting_cast<int>(v.size()),   // send_count
+            MPI_INT,                         // datatype
+            asserting_cast<int>(other_rank), // destination
+            0,                               // tag
+            comm.mpi_communicator(),         // comm
+            &req
+        );
+    }
+    if (comm.rank_shifted_cyclic(-1) == comm.root()) {
+        std::vector<int> message;
+        EXPECT_EQ(probe_counter, 0);
+        auto result = comm.recv(recv_buf(message), recv_counts(5), status_out());
+        EXPECT_FALSE(has_member_extract_recv_counts_v<decltype(result)>);
+        EXPECT_TRUE(has_member_extract_status_v<decltype(result)>);
+        auto status = result.extract_status();
+        // we should not probe for the message size inside of KaMPIng if we specify the recv count explicitly
+        EXPECT_EQ(probe_counter, 0);
+        EXPECT_EQ(status.source(), comm.root());
+        EXPECT_EQ(status.tag(), 0);
+        EXPECT_EQ(status.count<int>(), 5);
+        EXPECT_EQ(message, std::vector<int>({1, 2, 3, 4, 5}));
+    }
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+}
+
+TEST_F(RecvTest, recv_vector_with_input_status) {
+    Communicator comm;
+    std::vector  v{1, 2, 3, 4, 5};
+    MPI_Request  req = MPI_REQUEST_NULL;
+    if (comm.is_root()) {
+        auto other_rank = comm.rank_shifted_cyclic(1);
+        MPI_Isend(
+            v.data(),                        // send_buf
+            asserting_cast<int>(v.size()),   // send_count
+            MPI_INT,                         // datatype
+            asserting_cast<int>(other_rank), // destination
+            0,                               // tag
+            comm.mpi_communicator(),         // comm
+            &req
+        );
+    }
+    if (comm.rank_shifted_cyclic(-1) == comm.root()) {
+        std::vector<int> message;
+        Status           recv_status;
+        // pass status as input parameter
+        auto result = comm.recv(recv_buf(message), status(recv_status));
+        EXPECT_TRUE(has_member_extract_recv_counts_v<decltype(result)>);
+        EXPECT_FALSE(has_member_extract_status_v<decltype(result)>);
+        EXPECT_EQ(recv_status.source(), comm.root());
+        EXPECT_EQ(recv_status.tag(), 0);
+        EXPECT_EQ(recv_status.count<int>(), 5);
+        EXPECT_EQ(result.extract_recv_counts(), 5);
+        EXPECT_EQ(message, std::vector<int>({1, 2, 3, 4, 5}));
+    }
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+}
+
+TEST_F(RecvTest, recv_from_proc_null) {
+    Communicator comm;
+    std::vector  v{1, 2, 3, 4, 5};
+    auto         result = comm.recv(source(rank::null), recv_buf(v), status_out());
+    EXPECT_TRUE(has_member_extract_recv_counts_v<decltype(result)>);
+    auto   status     = result.extract_status();
+    size_t recv_count = static_cast<size_t>(result.extract_recv_counts());
+    // recv did not touch the buffer
+    EXPECT_EQ(v.size(), 5);
+    EXPECT_EQ(v, std::vector({1, 2, 3, 4, 5}));
+    EXPECT_EQ(status.source_signed(), MPI_PROC_NULL);
+    EXPECT_EQ(status.tag(), MPI_ANY_TAG);
+    EXPECT_EQ(status.template count<int>(), 0);
+    EXPECT_EQ(recv_count, 0);
+}
