f2f4c46fb41749cb57c72ca7313659e54f1f483d
Demian Hespe <hespe@kit.edu>
2022-12-12 16:49:30 +0100

    Implement for scatter

--- include/kamping/communicator.hpp
+++ include/kamping/communicator.hpp
@@ -262,7 +262,7 @@ public:
     template <typename... Args>
     auto alltoallv(Args... args) const;
 
-    template <typename... Args>
+    template <typename recv_value_type_tparam = kamping::internal::unused_tparam, typename... Args>
     auto scatter(Args... args) const;
 
     template <typename recv_value_type_tparam = kamping::internal::unused_tparam, typename... Args>
