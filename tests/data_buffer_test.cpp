99005e851e521658e9740207838bc396e42e8736
Demian Hespe <hespe@kit.edu>
2022-12-15 11:09:04 +0100

    Remove undefined behavior previously introduced

--- tests/data_buffer_test.cpp
+++ tests/data_buffer_test.cpp
@@ -17,7 +17,6 @@
 #include <vector>
 
 #include <gtest/gtest.h>
-#include <limits.h>
 
 #include "helpers_for_testing.hpp"
 #include "kamping/assertion_levels.hpp"
@@ -510,10 +509,9 @@ TEST(SingleElementModifiableBufferTest, get_basics) {
 }
 
 TEST(LibAllocatedSingleElementBufferTest, move_constructor_is_enabled) {
-    constexpr ParameterType ptype = ParameterType::send_counts;
-    constexpr BufferType    btype = BufferType::in_buffer;
-    int                     elem  = INT_MAX;
-    elem += 1;
+    constexpr ParameterType                            ptype      = ParameterType::send_counts;
+    constexpr BufferType                               btype      = BufferType::in_buffer;
+    int                                                elem       = 42;
     int const                                          const_elem = elem;
     LibAllocatedSingleElementBuffer<int, ptype, btype> buffer1{};
     *buffer1.get().data() = elem;
