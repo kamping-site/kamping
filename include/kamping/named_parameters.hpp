96d0ab531fc22aab5354bc9941f3a33d237fe595
Tim Niklas Uhl <uhl@kit.edu>
2023-07-17 09:16:00 +0200

    Add doxygen and container variants for `wait_all`.

--- include/kamping/named_parameters.hpp
+++ include/kamping/named_parameters.hpp
@@ -575,29 +575,31 @@ inline auto tag(EnumType value) {
     return tag(static_cast<int>(value));
 }
 
-///@brief Use the provided native \c MPI_Status as status parameter.
-///@param mpi_status The status.
+/// @brief Use the provided native \c MPI_Status as status parameter.
+/// @param mpi_status The status.
 inline auto status(MPI_Status& mpi_status) {
     return internal::StatusParam<internal::StatusParamType::native_ref>{mpi_status};
 }
 
-///@brief Use the  provided \ref kamping::Status as status parameter.
-///@param mpi_status The status.
+/// @brief Use the  provided \ref kamping::Status as status parameter.
+/// @param mpi_status The status.
 inline auto status(Status& mpi_status) {
     return internal::StatusParam<internal::StatusParamType::ref>(mpi_status);
 }
 
-///@brief Construct a status object internally, which may then be retrieved from \c kamping::MPIResult returned by the
+/// @brief Construct a status object internally, which may then be retrieved from \c kamping::MPIResult returned by the
 /// operation.
 inline auto status_out() {
     return internal::StatusParam<internal::StatusParamType::owning>{};
 }
 
-///@brief pass \c MPI_STATUS_IGNORE to the underlying MPI call.
+/// @brief pass \c MPI_STATUS_IGNORE to the underlying MPI call.
 inline auto status(internal::ignore_t<void>) {
     return internal::StatusParam<internal::StatusParamType::ignore>{};
 }
 
+/// @brief Pass a request handle to the underlying MPI call.
+/// @param request The request handle.
 inline auto request(Request& request) {
     return internal::make_data_buffer<
         internal::ParameterType::request,
@@ -605,6 +607,7 @@ inline auto request(Request& request) {
         internal::BufferType::out_buffer>(request);
 }
 
+/// @brief Internally allocate a request object and return it to the user.
 inline auto request() {
     return internal::make_data_buffer<
         internal::ParameterType::request,
--- include/kamping/request.hpp
+++ include/kamping/request.hpp
@@ -13,15 +13,24 @@
 
 #pragma once
 
+#include <algorithm>
+
 #include <kamping/error_handling.hpp>
 #include <mpi.h>
 
+#include "kamping/data_buffer.hpp"
+
 namespace kamping {
 
+/// @brief Wrapper for MPI request handles (aka. \c MPI_Request).
 class Request {
 public:
+    /// @brief Constructs a request handle from an \c MPI_Request.
+    /// @param request The request to encapsulate. Defaults to \c MPI_REQUEST_NULL.
     Request(MPI_Request request = MPI_REQUEST_NULL) : _request(request) {}
 
+    /// @brief Returns when the operation defined by the underlying request completes.
+    /// If the underlying request was intialized by a non-blocking communication call, it is set to \c MPI_REQUEST_NULL.
     void wait() {
         int err = MPI_Wait(&_request, MPI_STATUS_IGNORE);
         THROW_IF_MPI_ERROR(err, MPI_Wait);
@@ -32,6 +41,8 @@ public:
         return _request == MPI_REQUEST_NULL;
     }
 
+    /// @return Returns \c true if the underlying request is complete. In that case and if the underlying request was
+    /// initialized by a non-blocking communication call, it is set to \c MPI_REQUEST_NULL.
     [[nodiscard]] bool test() {
         int is_finished;
         int err = MPI_Test(&_request, &is_finished, MPI_STATUS_IGNORE);
@@ -44,12 +55,19 @@ public:
         return _request;
     }
 
+    /// @return A reference to the underlying MPI_Request handle.
+    [[nodiscard]] MPI_Request const& native() const {
+        return _request;
+    }
+
     // TODO: request cancellation and querying of cancelation status
 
+    /// @return Returns \c true if the other request wrapper points to the same request.
     bool operator==(Request const& other) const {
         return _request == other._request;
     }
 
+    /// @return Returns \c true if the other request wrapper points to a different request.
     bool operator!=(Request const& other) const {
         return !(*this == other);
     }
@@ -59,14 +77,49 @@ private:
 };
 
 namespace requests {
+
+/// @brief Waits for completion of all requests handles passed.
+/// @param requests A (contiguous) container of \c MPI_Request.
+/// @tparam Container The container type.
+template <
+    typename Container,
+    typename std::enable_if_t<
+        internal::has_data_member_v<Container> && std::is_same_v<typename Container::value_type, MPI_Request>,
+        bool>::type = true>
+void wait_all(Container& requests) {
+    int err = MPI_Waitall(asserting_cast<int>(requests.size()), requests.data(), MPI_STATUSES_IGNORE);
+    THROW_IF_MPI_ERROR(err, MPI_Waitall);
+}
+
+/// @brief Waits for completion of all requests handles passed.
+/// This incurs overhead for copying the request handles to an intermediate container.
+/// @param requests A (contiguous) container of \ref kamping::Request.
+/// @tparam Container The container type.
+template <
+    typename Container,
+    typename std::enable_if_t<
+        internal::has_data_member_v<Container> && std::is_same_v<typename Container::value_type, Request>,
+        bool>::type = true>
+void wait_all(Container const& requests) {
+    MPI_Request reqs[requests.size()];
+    auto        begin = requests.data();
+    auto        end   = begin + requests.size();
+    std::transform(begin, end, reqs.begin(), [](Request& req) { return req.native(); });
+    wait_all(kamping::Span(reqs, requests.size()));
+}
+
+/// @brief Wait for completion of all request handles passed.
+/// This incurs overhead for copying the request handles to an intermediate container.
+/// @param args A list of \ref kamping::Request object to wait on.
 template <typename... Requests, typename = std::enable_if_t<std::conjunction_v<std::is_same<Requests, Request>...>>>
 void wait_all(Requests /*Request*/&... args) {
     constexpr size_t req_size       = sizeof...(args);
     MPI_Request      reqs[req_size] = {args.native()...};
-    int              err            = MPI_Waitall(req_size, reqs, MPI_STATUSES_IGNORE);
-    THROW_IF_MPI_ERROR(err, MPI_Waitall);
+    wait_all(kamping::Span(reqs, req_size));
 }
+
 // TODO: wait_any, wait_same, test_all, test_any, test_some
+
 } // namespace requests
 
 } // namespace kamping
