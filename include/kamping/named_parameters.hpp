1366fecae68338b53cc6668f05550df593738b62
Tim Niklas Uhl <uhl@kit.edu>
2023-07-26 13:56:31 +0200

    Moved status parameters to separate header.

--- include/kamping/named_parameters.hpp
+++ include/kamping/named_parameters.hpp
@@ -24,6 +24,7 @@
 #include "kamping/mpi_datatype.hpp"
 #include "kamping/mpi_ops.hpp"
 #include "kamping/named_parameter_types.hpp"
+#include "kamping/named_parameters_detail/status_parameters.hpp"
 #include "kamping/operation_builder.hpp"
 #include "kamping/parameter_objects.hpp"
 #include "kamping/request.hpp"
@@ -569,29 +570,6 @@ inline auto tag(EnumType value) {
         "The underlying enum type must be implicitly convertible to int."
     );
     return tag(static_cast<int>(value));
-}
-
-/// @brief Use the provided native \c MPI_Status as status parameter.
-/// @param mpi_status The status.
-inline auto status(MPI_Status& mpi_status) {
-    return internal::StatusParam<internal::StatusParamType::native_ref>{mpi_status};
-}
-
-/// @brief Use the  provided \ref kamping::Status as status parameter.
-/// @param mpi_status The status.
-inline auto status(Status& mpi_status) {
-    return internal::StatusParam<internal::StatusParamType::ref>(mpi_status);
-}
-
-/// @brief Construct a status object internally, which may then be retrieved from \c kamping::MPIResult returned by the
-/// operation.
-inline auto status_out() {
-    return internal::StatusParam<internal::StatusParamType::owning>{};
-}
-
-/// @brief pass \c MPI_STATUS_IGNORE to the underlying MPI call.
-inline auto status(internal::ignore_t<void>) {
-    return internal::StatusParam<internal::StatusParamType::ignore>{};
 }
 
 /// @brief Pass a request handle to the underlying MPI call.
