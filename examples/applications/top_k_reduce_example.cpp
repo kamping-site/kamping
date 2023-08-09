537f301b2be08cd7d7f4703be6f85cecc8775861
Lukas Hübner <huebner@kit.edu>
2023-08-09 09:57:00 +0200

    Add comments to top-k-selection example
    - Make the plain MPI code more readable by commenting on which parameter is which.
    - Swap plain MPI and KaMPIng code (plain MPI above KaMPIng)
    - Add more comments on what KaMPIng does automatically
    - Rename some variables to make them more self-documenting

--- examples/applications/top_k_reduce_example.cpp
+++ examples/applications/top_k_reduce_example.cpp
@@ -16,9 +16,11 @@ template <size_t K, typename ValueType>
 class TopK {
 public:
     explicit TopK() noexcept {}
+
     ValueType& operator[](size_t i) {
         return elements[i];
     }
+
     ValueType const& operator[](size_t i) const {
         return elements[i];
     }
@@ -32,12 +34,12 @@ TopK<K, ValueType> merge(TopK<K, ValueType> const& lhs, TopK<K, ValueType> const
     size_t          lhs_current = 0;
     size_t          rhs_current = 0;
     TopK<K, size_t> merged;
-    for (size_t i = 0; i < K; i++) {
+    for (size_t output_idx = 0; output_idx < K; output_idx++) {
         if (lhs[lhs_current] < rhs[rhs_current]) {
-            merged[i] = lhs[lhs_current];
+            merged[output_idx] = lhs[lhs_current];
             lhs_current++;
         } else {
-            merged[i] = rhs[rhs_current];
+            merged[output_idx] = rhs[rhs_current];
             rhs_current++;
         }
     }
@@ -47,70 +49,99 @@ TopK<K, ValueType> merge(TopK<K, ValueType> const& lhs, TopK<K, ValueType> const
 template <size_t K, typename ValueType>
 std::ostream& operator<<(std::ostream& os, TopK<K, ValueType> const& top_k) {
     os << "TopK(";
-    for (size_t i = 0; i < K; ++i) {
-        if (i != 0) {
+    for (size_t idx = 0; idx < K; ++idx) {
+        if (idx != 0) {
             os << ", ";
         }
-        os << top_k[i];
+        os << top_k[idx];
     }
     os << ")";
     return os;
 }
 
-template <size_t K, typename ValueType>
-auto kamping_top_k(TopK<K, ValueType> const& local_top_k, kamping::BasicCommunicator& comm) {
-    using namespace kamping;
-    auto result = comm.reduce(send_buf(local_top_k), op(merge<K, size_t>, ops::commutative)).extract_recv_buffer();
-    if (comm.is_root()) {
-        return std::make_optional(result[0]);
-    } else {
-        return std::optional<TopK<K, ValueType>>{};
-    }
-}
-
+// Top-K reduction using plain MPI
 template <size_t K, typename ValueType>
 std::optional<TopK<K, ValueType>> mpi_top_k(TopK<K, ValueType> const& local_top_k, MPI_Comm comm) {
-    // create a custom datatype
+    // We first have to create a custom datatype:
     MPI_Datatype topK_type;
-    // to make it truly generic we rely on KaMPIng
-    MPI_Type_contiguous(K, kamping::mpi_datatype<ValueType>(), &topK_type);
+
+    // We rely on the automatic compile-time deduction of the datatype of the single elements (ValueType) via KaMPIng to
+    // make this code as generic as possible without adding another 300+ lines of code here.
+    // See kamping/mpi_datatype.hpp for the implementation of kamping::mpi_datatype.
+    MPI_Type_contiguous(
+        K,                                  // count
+        kamping::mpi_datatype<ValueType>(), // oldtype
+        &topK_type                          // newtype
+    );
     MPI_Type_commit(&topK_type);
 
-    // create a custom reduce operation
+    // Second, we have to register our custom reduce operation with MPI:
     MPI_Op             topK_merge_op;
     MPI_User_function* merge_op = [](void* invec, void* inoutvec, int* len, MPI_Datatype*) {
         TopK<K, ValueType>* invec_    = static_cast<TopK<K, ValueType>*>(invec);
         TopK<K, ValueType>* inoutvec_ = static_cast<TopK<K, ValueType>*>(inoutvec);
-        std::transform(invec_, invec_ + *len, inoutvec_, inoutvec_, merge<K, size_t>);
+        std::transform(
+            invec_,          // first1  (input)
+            invec_ + *len,   // last1   (input)
+            inoutvec_,       // first2  (input)
+            inoutvec_,       // d_first (output)
+            merge<K, size_t> // binary_op
+        );
     };
-    MPI_Op_create(merge_op, true, &topK_merge_op);
+    MPI_Op_create(
+        merge_op,      // function
+        true,          // commutative
+        &topK_merge_op // op
+    );
 
-    // the actual MPI call
+    // Next, perform the actual communication using plain MPI
     TopK<K, ValueType> global_top_k;
-    MPI_Reduce(&local_top_k, &global_top_k, 1, topK_type, topK_merge_op, 0, comm);
+    MPI_Reduce(
+        &local_top_k,  // sendbuf
+        &global_top_k, // recvbuf
+        1,             // count
+        topK_type,     // datatype
+        topK_merge_op, // op
+        0,             // root,
+        comm
+    );
 
-    // cleanup
+    // Finally, clean up the custom datatype and reduce operation.
     MPI_Op_free(&topK_merge_op);
     MPI_Type_free(&topK_type);
 
     int rank;
     MPI_Comm_rank(comm, &rank);
-    if (rank == 0) {
-        return std::make_optional(std::move(global_top_k));
-    } else {
-        return std::optional<TopK<K, ValueType>>{};
-    }
+    return rank == 0 ? std::make_optional(std::move(global_top_k)) : std::optional<TopK<K, ValueType>>{};
+}
+
+// Top-K reduction using KaMPIng
+template <size_t K, typename ValueType>
+std::optional<TopK<K, ValueType>>
+kamping_top_k(TopK<K, ValueType> const& local_top_k, kamping::BasicCommunicator& comm) {
+    using namespace kamping;
+
+    // KaMPIng simplifies the callsite by:
+    // - Providing a named parameter interface.
+    // - Defaulting to using rank 0 as root.
+    // - Automatically deducing the datatype of the elements.
+    // - Automatically creating and de-creating the custom datatype and the reduce operation.
+    auto result = comm.reduce(send_buf(local_top_k), op(merge<K, size_t>, ops::commutative)).extract_recv_buffer();
+
+    return comm.is_root() ? std::make_optional(result[0]) : std::optional<TopK<K, ValueType>>{};
 }
 
 int main(int argc, char* argv[]) {
     namespace kmp = kamping;
+
+    // KaMPIng wraps MPI_Init and MPI_Finalize using the RAII principle.
     kmp::Environment       env(argc, argv);
     kmp::BasicCommunicator comm;
 
     constexpr size_t K = 3;
     TopK<K, size_t>  input;
-    for (size_t i = 0; i < K; ++i) {
-        input[i] = comm.rank() + i * comm.size();
+    for (size_t idx = 0; idx < K; ++idx) {
+        input[idx] = comm.rank() + idx * comm.size();
     }
     std::cout << "[R" << comm.rank() << "] local_input=" << input << std::endl;
 
