6df87435ded17b657b9bb40cc2eca81c1e847c20
Lukas Hübner <huebner@kit.edu>
2023-08-11 16:24:39 +0200

    Merge

--- include/kamping/comm_helper/num_numa_nodes.hpp
+++ include/kamping/comm_helper/num_numa_nodes.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "kamping/communicator.hpp"
 #include "kamping/collectives/allreduce.hpp"
+#include "kamping/communicator.hpp"
 
 template <template <typename...> typename DefaultContainerType, template <typename> typename... Plugins>
 size_t kamping::Communicator<DefaultContainerType, Plugins...>::num_numa_nodes() const {
@@ -12,7 +12,8 @@ size_t kamping::Communicator<DefaultContainerType, Plugins...>::num_numa_nodes()
     size_t const numa_representative = numa_comm.allreduce_single(send_buf(rank()), op(ops::min<>{}));
 
     // Determine the number of NUMA nodes by counting the number of distinct lowest ranks.
-    size_t const num_numa_nodes = allreduce_single(send_buf(numa_representative == rank() ? 1 : 0), op(ops::plus<>{}));
+    size_t const num_numa_nodes =
+        allreduce_single(send_buf(numa_representative == rank() ? 1ul : 0ul), op(ops::plus<>{}));
 
     return num_numa_nodes;
 }
