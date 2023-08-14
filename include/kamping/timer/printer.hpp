0e627a4ad597d3b79f9d6b0fb2e9d7966cc497e6
Matthias Schimek <matthias.schimek@kit.edu>
2023-08-14 17:17:14 +0200

    add customizable outstream to printer

--- include/kamping/timer/printer.hpp
+++ include/kamping/timer/printer.hpp
@@ -27,6 +27,13 @@ inline std::string quote_string(std::string const& str) {
 /// @brief Printer class that prints an evaluated TimerTree in Json format.
 class SimpleJsonPrinter {
 public:
+    /// @brief Construct a printer that use std::cout as outstream.
+    SimpleJsonPrinter() : _outstream{std::cout} {}
+
+    /// @brief Construct a printer printing to a given outstream.
+    ///
+    /// @param outstream Outstream on which the content is printed.
+    SimpleJsonPrinter(std::ostream& outstream) : _outstream{outstream} {}
     /// @brief Prints an evaluated TimerTree in Json format to stdout.
     /// @tparam Duration Type to represent a duration.
     /// @param node Root node of the TimerTree to print.
@@ -36,70 +43,73 @@ public:
         const std::size_t indentation_per_level = 2;
         auto              name                  = node.name();
         auto              evaluation_data       = node.aggregated_data();
-        std::cout << std::string(indentation, ' ') << quote_string(name) << ": {" << std::endl;
+        _outstream << std::string(indentation, ' ') << quote_string(name) << ": {" << std::endl;
 
-        InternalPrinter<Duration> internal_printer;
-        std::cout << std::string(indentation + indentation_per_level, ' ') << quote_string("statistics") << ": {"
-                  << std::endl;
+        InternalPrinter<Duration> internal_printer{_outstream};
+        _outstream << std::string(indentation + indentation_per_level, ' ') << quote_string("statistics") << ": {"
+                   << std::endl;
         if (!evaluation_data.empty()) {
             bool is_first_outer = true;
             for (auto const& [op, data]: evaluation_data) {
                 if (!is_first_outer) {
-                    std::cout << "," << std::endl;
+                    _outstream << "," << std::endl;
                 }
                 is_first_outer = false;
-                std::cout << std::string(indentation + 2 * indentation_per_level, ' ') << "\"" << op << "\""
-                          << ": [";
+                _outstream << std::string(indentation + 2 * indentation_per_level, ' ') << "\"" << op << "\""
+                           << ": [";
                 bool is_first = true;
                 for (auto const& data_item: data) {
                     if (!is_first) {
-                        std::cout << ", ";
+                        _outstream << ", ";
                     }
                     is_first = false;
                     std::visit(internal_printer, data_item);
                 }
-                std::cout << "]";
+                _outstream << "]";
             }
-            std::cout << std::endl;
+            _outstream << std::endl;
         }
-        std::cout << std::string(indentation + indentation_per_level, ' ') << "}";
+        _outstream << std::string(indentation + indentation_per_level, ' ') << "}";
         if (!node.children().empty()) {
-            std::cout << ",";
+            _outstream << ",";
         }
-        std::cout << std::endl;
+        _outstream << std::endl;
 
         bool is_first = true;
         for (auto const& children: node.children()) {
             if (!is_first) {
-                std::cout << "," << std::endl;
+                _outstream << "," << std::endl;
             }
             is_first = false;
             print(*children, indentation + indentation_per_level);
         }
         if (!node.children().empty()) {
-            std::cout << std::endl;
+            _outstream << std::endl;
         }
-        std::cout << std::string(indentation, ' ') << "}";
+        _outstream << std::string(indentation, ' ') << "}";
     }
 
 private:
+    std::ostream& _outstream; ///< Outstream to print on.
     template <typename T>
     struct InternalPrinter {
+        InternalPrinter(std::ostream& outstream) : _outstream{outstream} {}
         void operator()(std::vector<T> const& vec) const {
-            std::cout << "[";
+            _outstream << "[";
             bool is_first = true;
             for (auto const& elem: vec) {
                 if (!is_first) {
-                    std::cout << ", ";
+                    _outstream << ", ";
                 }
                 is_first = false;
-                std::cout << std::fixed << elem;
+                _outstream << std::fixed << elem;
             }
-            std::cout << "]";
+            _outstream << "]";
         }
         void operator()(T const& scalar) const {
-            std::cout << std::fixed << scalar;
+            _outstream << std::fixed << scalar;
         }
+        std::ostream& _outstream;
     };
 };
 
