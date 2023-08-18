de5572e900d36b1ebf2a0ee0bb1802ababec0655
Lukas Hübner <huebner@kit.edu>
2023-08-18 11:27:25 +0200

    Rewrite the sparse-alltoall example using the new Communicator::try_recv

--- examples/applications/sparse_all_to_all_example.cpp
+++ examples/applications/sparse_all_to_all_example.cpp
@@ -10,39 +10,32 @@
 
 // A very non-generic implementation of a sparse all-to-all communication that is conceptually similar to the generic
 // one provided via kamping::Mailbox
+template <typename T, typename Comm>
 class SparseAllToAll {
-    template <typename T, typename Comm>
-    std::unordered_map<size_t, std::vector<T>>
+    struct RecvdMessage {
+        std::vector<T> data;
+        size_t         src_rank;
+    };
+
+    std::vector<RecvdMessage>
     alltoall(Comm& comm, std::unordered_map<size_t, std::vector<T>> const& send_messages, int const _tag) {
         using namespace ::kamping;
 
-        std::unordered_map<size_t, std::vector<T>> recvd_messages;
+        std::vector<RecvdMessage> recvd_messages;
+        recvd_messages.emplace_back({});
 
-        // TODO Extract this into a convenience function in kamping::Communicator
-        // Define the function to receive the next message if one is available
-        auto recv_msg_if_avail =
-            [](Comm& comm, std::unordered_map<size_t, std::vector<T>>& recvd_messages, int const _tag) {
-                MPI_Status recv_status;
+        // Checks if a new message is available for receiving and if so, receives into recvd_messages.
+        auto const recv_msg_if_avail = [&comm, &recvd_messages, _tag]() {
+            static Status recv_status;
+            auto const&   current_msg = recvd_messages.back();
 
-                bool const msg_avail = comm.iprobe(source(rank::any), tag(_tag), status(recv_status));
-                if (msg_avail) {
-                    auto const _source = recv_status.MPI_SOURCE;
-                    KASSERT(
-                        recv_status.MPI_TAG == _tag,
-                        "Tag of the iprobe-returned status does not match the requested tag.",
-                        assert::light
-                    );
-
-                    KASSERT(
-                        recvd_messages.find(_source) == recvd_messages.end(),
-                        "Received a message from a source that has already sent a message.",
-                        assert::light
-                    );
-                    recvd_messages[_source].emplace({});
-                    auto const result =
-                        comm.recv(source(_source), tag(_tag), recv_buf(recvd_messages[_source]).status(recv_status));
-                }
-            };
+            bool const msg_received =
+                comm.try_recv(recv_buf(current_msg.data), tag(_tag), source(rank::any), status(recv_status));
+            if (msg_received) {
+                current_msg.src_rank = recv_status.source();
+                recvd_messages.emplace_back({});
+            }
+        };
 
         // Send out all our messages using issend; keep the corresponding requests around for checking if the messages
         // have been received.
@@ -55,8 +48,7 @@ class SparseAllToAll {
         // TODO Rewrite this, once we have a request pool
         // Receive messages until all messages sent have been received
         while (!kamping::requests::test_all(send_msg_requests)) {
-            // If there's a message available, receive it directly into recv_messages
-            recv_msg_if_avail(comm, recvd_messages, tag);
+            recv_msg_if_avail();
         }
 
         // Enter a ibarrier; this signals to all other PEs that all messages we sent were received.
@@ -65,9 +57,13 @@ class SparseAllToAll {
         // Continue receiving messages sent to us until all ranks posted to the barrier that their (and thus all)
         // messages have been received.
         while (!ibarrier.test()) {
-            recv_msg_if_avail(comm, recvd_messages, tag);
+            recv_msg_if_avail();
         }
 
+        // Remove the last (empty) message from recvd_messages and return.
+        KASSERT(!recvd_messages.empty());
+        KASSERT(recvd_messages.back().data.empty());
+        recvd_messages.pop_back();
         return recvd_messages;
     }
 };
