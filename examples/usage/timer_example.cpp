f39aa3c878f01fcd3ab5524b406756307d6e08cf
Matthias Schimek <matthias.schimek@kit.edu>
2023-08-20 21:39:58 +0200

    add some comments in timer example

--- examples/usage/timer_example.cpp
+++ examples/usage/timer_example.cpp
@@ -44,19 +44,30 @@ int main() {
         for (volatile int i = 0; i < it; ++i)
             ;
     };
+    // Get timer singleton. Alternatively you can also instantiate a new timer.
     auto& t = kamping::measurements::timer();
+    // Synchronizes all ranks via a call to MPI_Barrier and then start the measurement with key "algorithm".
     t.synchronize_and_start("algorithm");
     for (size_t i = 0; i < 3; ++i) {
         t.synchronize_and_start("round" + std::to_string(i));
         {
+            // Synchronizes all ranks and then start a measurement with key "preprocessing". The enclosing measurement
+            // "algorithm" is still active.
             t.synchronize_and_start("preprocessing");
             sleep_some_time();
+            // Stops the measurement "preprocessing". As no communicator wide duration aggregation operation is given
+            // the default "max" is chosen to be applied later during evaluation.
             t.stop();
 
             t.synchronize_and_start("core_algorithm");
             for (size_t j = 0; j < 5u; ++j) {
+                // Starts the measurement "subroutine" without synchronizing all ranks
                 t.start("subroutine");
                 sleep_some_time();
+                // Stops the measurement "subroutine" and specifies that the communicator wide duration aggregation
+                // operation "min", "max" and "gather" will be applied to this measurement.
+                // Furthermore, the measured durations are stored in a list instead of being summed up (- note that this
+                // measurement with key "subroutine" will be executed 3*5 times)
                 t.stop_and_append(
                     {measurements::DataAggregationMode::min,
                      measurements::DataAggregationMode::max,
@@ -71,6 +82,7 @@ int main() {
         t.stop_and_append();
     }
     t.stop();
+    // Evaluates the timer and prints the aggregated duration using the print() method from SimpleJsonPrinter.
     t.evaluate_and_print(kamping::measurements::SimpleJsonPrinter{});
     std::cout << std::endl;
     t.evaluate_and_print(kamping::measurements::FlatPrinter{});
