d1dd3ffbc6fe74c6eb73dac639e720724bd61498
Niklas Uhl <tim.uhl@kit.edu>
2024-01-31 15:48:36 +0100

    Update README with content from brief announcement. (#616)
    * Start updating README.
    
    * Add sorting example.
    
    * Fix link.
    
    * Actually fix the link.
    
    * Add plot to README.
    
    * Comparision table.
    
    * Complete table.
    
    * Aligment.
    
    * Fix GCC build for sorting example.
    
    * Update README.md
    
    Co-authored-by: Florian Kurpicz <florian@kurpicz.org>
    
    * Update README.md
    
    Co-authored-by: Florian Kurpicz <florian@kurpicz.org>
    
    * Update README.md
    
    Co-authored-by: Florian Kurpicz <florian@kurpicz.org>
    
    * Update README.md
    
    Co-authored-by: Florian Kurpicz <florian@kurpicz.org>
    
    * Also fill vector in kamping gatherv example.
    
    * More details on resize policies.
    
    * Better logo usage in README.
    
    ---------
    
    Co-authored-by: Florian Kurpicz <florian@kurpicz.org>

---
+++ examples/applications/sample-sort/kamping.hpp
@@ -0,0 +1,39 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2024 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+#pragma once
+#include <random>
+
+#include <kamping/collectives/allgather.hpp>
+#include <kamping/collectives/alltoall.hpp>
+#include <kamping/communicator.hpp>
+
+#include "./common.hpp"
+namespace kamping {
+template <typename T>
+void sort(MPI_Comm comm_, std::vector<T>& data, size_t seed) {
+    Communicator<> comm(comm_);
+    size_t const   oversampling_ratio = 16 * static_cast<size_t>(std::log2(comm.size())) + 1;
+    std::vector<T> local_samples(oversampling_ratio);
+    std::sample(data.begin(), data.end(), local_samples.begin(), oversampling_ratio, std::mt19937{seed});
+    auto global_samples = comm.allgather(send_buf(local_samples)).extract_recv_buffer();
+    pick_splitters(comm.size() - 1, oversampling_ratio, global_samples);
+    auto             buckets = build_buckets(data, global_samples);
+    std::vector<int> scounts;
+    for (auto& bucket: buckets) {
+        data.insert(data.end(), bucket.begin(), bucket.end());
+        scounts.push_back(static_cast<int>(bucket.size()));
+    }
+    data = comm.alltoallv(send_buf(data), send_counts(scounts)).extract_recv_buffer();
+    std::sort(data.begin(), data.end());
+}
+} // namespace kamping
