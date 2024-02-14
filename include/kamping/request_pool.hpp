7c9ec345f900064970f8a772d667de462ef0003c
Niklas Uhl <tim.uhl@kit.edu>
2024-02-14 11:28:51 +0100

    Implement `test_any` and `wait_any` (#625)
    * Implement and test `wait_any`.
    
    * Return `index_end()` instead of optional for index `MPI_UNDEFINED`.
    
    * Implement `test_any`.
    
    * Tests for `test_any`.
    
    * Fix only some very small issues.

--- include/kamping/request_pool.hpp
+++ include/kamping/request_pool.hpp
@@ -1,3 +1,17 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2024 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#pragma once
 #pragma once
 
 #include <cstddef>
@@ -13,6 +27,16 @@
 
 namespace kamping {
 
+/// @brief Result returned by \ref RequestPool.wait_any()
+/// @tparam IndexType Type of the stored Index.
+/// @tparam StatusType Type of the status object.
+template <typename IndexType, typename StatusType>
+struct PoolAnyResult {
+    IndexType
+        index; ///< The index of the completed operation. \ref RequestPool.index_end() if there were no active requests.
+    StatusType status; ///< The status of the complete operation.
+};
+
 /// @brief A pool for storing multiple \ref Request s and checking them for completion.
 ///
 /// Requests are internally stored in a vector. The vector is resized as needed.
@@ -23,6 +47,16 @@ public:
     RequestPool() {}
 
     using index_type = size_t; ///< The type used to index requests in the pool.
+
+    /// @brief The first index value. The pool is empty if `index_begin() == index_end()`.
+    index_type index_begin() const {
+        return 0;
+    }
+
+    /// @brief The index value after the last one. The pool is empty if `index_begin() == index_end()`.
+    index_type index_end() const {
+        return _requests.size();
+    }
 
     /// @brief Returns the number of requests currently stored in the pool.
     size_t num_requests() const {
@@ -114,6 +148,91 @@ public:
             }
         } else {
             return static_cast<bool>(succeeded);
+        }
+    }
+
+    /// @brief Waits any request in the pool to complete by calling \c MPI_Waitany.
+    /// @param status A \c status parameter object to which the status information about the completed operation is
+    /// written. Defaults to \c kamping::status(ignore<>).
+    /// @return By default, returns  the index of the completed operation. If the pool is
+    /// empty or no request in the pool is active, returns an index equal to `index_end()`. If \p status is an owning
+    /// out parameter, also returns the status alongside the index by returning a \ref PoolAnyResult.
+    /// @see PoolAnyResult
+    template <typename StatusParamObjectType = decltype(status(ignore<>))>
+    auto wait_any(StatusParamObjectType status = kamping::status(ignore<>)) {
+        static_assert(
+            StatusParamObjectType::parameter_type == internal::ParameterType::status,
+            "Only status parameters are allowed."
+        );
+        int index;
+        int err = MPI_Waitany(
+            asserting_cast<int>(num_requests()),
+            request_ptr(),
+            &index,
+            internal::status_param_to_native_ptr(status)
+        );
+        THROW_IF_MPI_ERROR(err, MPI_Waitany);
+        if constexpr (internal::is_extractable<decltype(status)>) {
+            using status_type = decltype(status.extract());
+            if (index == MPI_UNDEFINED) {
+                return PoolAnyResult<index_type, status_type>{index_end(), status.extract()};
+            } else {
+                return PoolAnyResult<index_type, status_type>{static_cast<index_type>(index), status.extract()};
+            }
+        } else {
+            if (index == MPI_UNDEFINED) {
+                return index_end();
+            } else {
+                return static_cast<index_type>(index);
+            }
+        }
+    }
+
+    /// @brief Tests if any request in the pool is completed by calling \c MPI_Testany.
+    /// @param status A \c status parameter object to which the status information about the completed operation is
+    /// written. Defaults to \c kamping::status(ignore<>).
+    /// @return If any request completes, returns an `std::optional` containing information about the completed request.
+    /// Otherwise, `std::nullopt`. The value contained inside the optional depends on \p status parameter and
+    /// follows the same rules as for \ref wait_any.
+    /// @see wait_any
+    template <typename StatusParamObjectType = decltype(status(ignore<>))>
+    auto test_any(StatusParamObjectType status = kamping::status(ignore<>)) {
+        static_assert(
+            StatusParamObjectType::parameter_type == internal::ParameterType::status,
+            "Only status parameters are allowed."
+        );
+        int index;
+        int flag;
+        int err = MPI_Testany(
+            asserting_cast<int>(num_requests()),
+            request_ptr(),
+            &index,
+            &flag,
+            internal::status_param_to_native_ptr(status)
+        );
+        THROW_IF_MPI_ERROR(err, MPI_Testany);
+        if constexpr (internal::is_extractable<decltype(status)>) {
+            using status_type = decltype(status.extract());
+            using return_type = PoolAnyResult<index_type, status_type>;
+            if (flag) {
+                if (index == MPI_UNDEFINED) {
+                    return std::optional<return_type>{{index_end(), status.extract()}};
+                } else {
+                    return std::optional<return_type>{{static_cast<index_type>(index), status.extract()}};
+                }
+            } else {
+                return std::optional<PoolAnyResult<index_type, status_type>>{};
+            }
+        } else {
+            if (flag) {
+                if (index == MPI_UNDEFINED) {
+                    return std::optional{index_end()};
+                } else {
+                    return std::optional{static_cast<index_type>(index)};
+                }
+            } else {
+                return std::optional<index_type>{};
+            }
         }
     }
 
