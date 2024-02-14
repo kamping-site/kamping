44eb96209b492f1be3608b49eb58ff4307de3fad
Tim Niklas Uhl <uhl@kit.edu>
2024-02-14 11:16:47 +0100

    Fix only some very small issues.

--- include/kamping/request_pool.hpp
+++ include/kamping/request_pool.hpp
@@ -1,3 +1,17 @@
+// This file is part of KaMPIng.
+//
+// Copyright 2024 The KaMPIng Authors
+//
+// KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version. KaMPIng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with KaMPIng.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#pragma once
 #pragma once
 
 #include <cstddef>
@@ -178,7 +192,7 @@ public:
     /// @param status A \c status parameter object to which the status information about the completed operation is
     /// written. Defaults to \c kamping::status(ignore<>).
     /// @return If any request completes, returns an `std::optional` containing information about the completed request.
-    /// Otherwise, `std::nullopt`. The value contained inside the optional is dependent on \p status parameter and
+    /// Otherwise, `std::nullopt`. The value contained inside the optional depends on \p status parameter and
     /// follows the same rules as for \ref wait_any.
     /// @see wait_any
     template <typename StatusParamObjectType = decltype(status(ignore<>))>
