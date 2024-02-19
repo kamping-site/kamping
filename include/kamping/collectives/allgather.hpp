d2385b8d85700a65600079afd9e489896d03134d
Tim Niklas Uhl <uhl@kit.edu>
2024-02-19 14:21:35 +0100

    Update copyright year.

--- include/kamping/collectives/allgather.hpp
+++ include/kamping/collectives/allgather.hpp
@@ -1,6 +1,6 @@
 // This file is part of KaMPIng.
 //
-// Copyright 2022-2023 The KaMPIng Authors
+// Copyright 2022-2024 The KaMPIng Authors
 //
 // KaMPIng is free software : you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
