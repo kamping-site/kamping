34238857b54558e480368f116deb1f038b544ed2
Tim Niklas Uhl <uhl@kit.edu>
2024-03-06 14:40:44 +0100

    Fix build (?).

--- include/kamping/serialization.hpp
+++ include/kamping/serialization.hpp
@@ -119,7 +119,7 @@ auto deserialization_repack(BufferType buffer) {
         serialization_data.deserialize();
         return std::move(serialization_data).extract();
     } else {
-        std::move(buffer);
+        return std::move(buffer);
     }
 }
 } // namespace internal
