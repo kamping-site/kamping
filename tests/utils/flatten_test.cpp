f8be3398bf42501fa1de0ba0a3b1a3a872e7b198
Tim Niklas Uhl <uhl@kit.edu>
2024-03-07 09:41:00 +0100

    Fix test.

--- tests/utils/flatten_test.cpp
+++ tests/utils/flatten_test.cpp
@@ -6,6 +6,8 @@
 #include <kamping/communicator.hpp>
 #include <kamping/utils/flatten.hpp>
 
+#include "../helpers_for_testing.hpp"
+
 using namespace kamping;
 
 TEST(FlattenTest, basic) {
@@ -15,25 +17,13 @@ TEST(FlattenTest, basic) {
         sparse_send_buf.emplace(dst, std::vector<int>(1, dst));
     }
 
-    auto [recv_buf, recv_displs] = with_flattened(sparse_send_buf, comm).do_([&](auto... flattened) {
-        return comm.alltoallv(std::move(flattened)..., recv_counts_out());
+    auto [recv_buf, recv_counts, recv_displs] = with_flattened(sparse_send_buf, comm).do_([&](auto... flattened) {
+        return comm.alltoallv(std::move(flattened)..., recv_counts_out(), recv_displs_out());
     });
-    // print recv_buf
-    for (auto const& elem: recv_buf) {
-        std::cout << elem << " ";
-    }
-    std::cout << std::endl;
 
-    kamping::Span recv_buf_view(recv_buf);
-    for (size_t i = 0; i < comm.size(); i++) {
-        size_t chunk_size;
-        if (i == comm.size() - 1) {
-            chunk_size = recv_buf_view.size() - asserting_cast<size_t>(recv_displs[i]);
-        } else {
-            chunk_size = asserting_cast<size_t>(recv_displs[i + 1] - recv_displs[i]);
-        }
-        auto msg_from_rank_i = recv_buf_view.subspan(static_cast<size_t>(recv_displs[i]), chunk_size);
-        EXPECT_EQ(msg_from_rank_i.size(), 1);
-        EXPECT_THAT(msg_from_rank_i, ::testing::Each(comm.rank_signed()));
-    }
+    EXPECT_EQ(recv_buf.size(), comm.size());
+    EXPECT_THAT(recv_buf, ::testing::Each(comm.rank_signed()));
+    EXPECT_THAT(recv_counts.size(), comm.size());
+    EXPECT_THAT(recv_counts, ::testing::Each(1));
+    EXPECT_EQ(recv_displs, testing::iota_container_n(comm.size(), 0));
 }
