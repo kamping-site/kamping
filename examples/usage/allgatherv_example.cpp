b48471b2524d32f7d17c9dc81c5de74e7b85db51
Lukas Hübner <huebner@kit.edu>
2024-03-19 14:03:21 +0100

    Rework the allgatherv example
    The allgatherv example is designed as the first example the user should read. It does not assume any prior knowledge of terminology. It should highlight the main features of the KaMPIng API.

--- examples/usage/allgatherv_example.cpp
+++ examples/usage/allgatherv_example.cpp
@@ -27,44 +27,59 @@
 
 int main() {
     using namespace kamping;
-    kamping::Environment  e;
+
+    // The Environment class is a RAII wrapper around MPI_Init and MPI_Finalize.
+    kamping::Environment e;
+
+    // A kamping::Communicator abstracts away an MPI_Comm; here MPI_COMM_WORLD.
     kamping::Communicator comm;
-    std::vector<int>      input(comm.rank(), comm.rank_signed());
 
-    {
-        // simply return the recv buffer
+    // Note, that the size of the input vector is different for each rank.
+    std::vector<int> input(comm.rank(), comm.rank_signed());
+
+    { // Gather the input from all ranks to the root rank.
         auto recv_buffer = comm.allgatherv(send_buf(input));
-        print_result(recv_buffer, comm);
     }
 
-    {
-        // return recv buffer and recv_counts
+    { // We can also request the number of elements received from each rank. The recv_buf will always be the first out
+      // parameter. After that, the output parameters are ordered as they are in the function call.
         auto [recv_buffer, recv_counts] = comm.allgatherv(send_buf(input), recv_counts_out());
-        print_result(recv_buffer, comm);
-        print_result(recv_counts, comm);
     }
 
-    {
-        // write result to an exisiting container
+    { // To re-use memory, we can provide an already allocated container to the MPI call.
         std::vector<int> recv_buffer;
+        // Let KaMPIng resize the recv_buffer to the correct size. Other possibilities are no_resize and grow_only.
         comm.allgatherv(send_buf(input), recv_buf<resize_to_fit>(recv_buffer));
-        print_result(recv_buffer, comm);
 
-        // additionally, receive counts and/or receive displacements can be provided
+        // We can also re-use already allocated containers for the other output parameters, e.g. recv_counts.
         std::vector<int> recv_counts(comm.size());
         std::iota(recv_counts.begin(), recv_counts.end(), 0);
+        comm.allgatherv(send_buf(input), recv_buf<resize_to_fit>(recv_buffer), kamping::recv_counts(recv_counts));
+
         std::vector<int> recv_displs(comm.size());
         std::exclusive_scan(recv_counts.begin(), recv_counts.end(), recv_displs.begin(), 0);
         recv_buffer.clear();
 
+        // In this example, we combine all of the concepts mentioned above:
+        // - Se input as the send buffer
+        // - Receive all elements into recv_buffer, resizing it to fit exactly the number of elements received.
+        // - Output the number of elements received from each rank into recv_counts.
+        // - Output the displacement of the first element received from each rank into recv_displs.
         comm.allgatherv(
             send_buf(input),
             recv_buf<resize_to_fit>(recv_buffer),
             kamping::recv_counts(recv_counts),
             kamping::recv_displs(recv_displs)
         );
-        print_result(recv_buffer, comm);
-
-        return 0;
     }
+
+    { // If we have many out parameters, we can replace the  structured bindings with extract_*() calls in order to
+      // increase readability.
+        auto       result      = comm.allgatherv(send_buf(input), recv_counts_out(), recv_displs_out());
+        auto const recv_buffer = result.extract_recv_buffer();
+        auto const recv_counts = result.extract_recv_counts();
+        auto const recv_displs = result.extract_recv_displs();
+    }
+
+    return 0;
 }
