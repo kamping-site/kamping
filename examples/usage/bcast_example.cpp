681f4489f85150dbbe3baf8c613a393ae616f9da
Lukas Hübner <huebner@kit.edu>
2024-03-22 08:29:54 +0100

    Add `bcast_single` and `bcast(send_recv_buf(alloc_new<...>))` examples

--- examples/usage/bcast_example.cpp
+++ examples/usage/bcast_example.cpp
@@ -34,11 +34,35 @@ int main() {
     size_t value = comm.rank();
     comm.bcast(send_recv_buf(value));
 
+    // Sometimes the root and the other ranks call broadcast from different program paths.
+    // Without a `send_recv_buf`:
+    // - KaMPIng cannot deduce the receive type and we have to explicitly provide the C++ type using a template
+    //   parameter.
+    // - `bcast` has to way to decide at compile time if it will receive a single value or a container and will
+    //   therefore default to a container (possibly containing only a single element)
+    std::vector<size_t> values;
+    if (comm.is_root()) {
+        values = {42, 1337};
+        comm.bcast(send_recv_buf(values));
+    } else {
+        // Let KaMPIng allocate the receive buffer.
+        values = comm.bcast(send_recv_buf(alloc_new<std::vector<size_t>>));
+    }
+
+    // `bcast_single` is syntactic sugar for `bcast(..., send_recv_count(1))`.
+    if (comm.is_root()) {
+        comm.bcast_single(send_recv_buf(42));
+    } else {
+        // Provide the receive type via a template parameter.
+        value = comm.bcast_single<size_t>();
+    }
+
     // Broadcast a vector of values from the root rank to all other ranks. If we do not provide `send_recv_count`,
-    // KaMPIng automatically performs the second broadcast necessary to provideall ranks with the correct receive count.
-    // This is useful if some ranks do not know how many elements they will receive.
-    // Additionally, use rank 1 as the root rank here.
-    std::vector<int> values(4);
-    std::fill(values.begin(), values.end(), comm.rank());
+    // KaMPIng automatically performs the second broadcast necessary to provideall ranks with the correct receive
+    // count. This is useful if some ranks do not know how many elements they will receive. Additionally, use rank 1
+    // as the root rank here.
+    values.resize(4);
+    std::iota(values.begin(), values.end(), comm.rank());
+
     comm.bcast(send_recv_buf(values), send_recv_count(4), root(1));
 }
