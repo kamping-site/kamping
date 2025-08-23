6c0036b4ca5fdd79cfc88f57ac7803020bc3e6df
Niklas Uhl <uhl@kit.edu>
2025-08-23 13:22:12 +0200

    Use upstream KaTestrophe.

--- tests/cmake/KampingTestHelper.cmake
+++ tests/cmake/KampingTestHelper.cmake
@@ -1,5 +1,56 @@
-include(KaTestrophe)
-include(GoogleTest)
+include(FetchContent)
+FetchContent_Declare(
+    KaTestrophe
+    GIT_REPOSITORY https://github.com/kamping-site/KaTestrophe.cmake
+    GIT_TAG v1.0.2
+)
+FetchContent_MakeAvailable(KaTestrophe)
+
+# Registers a set of tests which should fail to compile.
+#
+# TARGET prefix for the targets to be built FILES the list of files to include in the target SECTIONS sections of the
+# compilation test to build LIBRARIES libraries to link via target_link_libraries(...)
+#
+# Loosely based on: https://stackoverflow.com/questions/30155619/expected-build-failure-tests-in-cmake
+function (katestrophe_add_compilation_failure_test)
+    cmake_parse_arguments(
+        "KATESTROPHE" # prefix
+        "" # options
+        "TARGET" # one value arguments
+        "FILES;SECTIONS;LIBRARIES" # multiple value arguments
+        ${ARGN}
+    )
+
+    # the file should compile without any section enabled
+    add_executable(${KATESTROPHE_TARGET} ${KATESTROPHE_FILES})
+    target_link_libraries(${KATESTROPHE_TARGET} PUBLIC GTest::gtest ${KATESTROPHE_LIBRARIES})
+
+    # For each given section, add a target.
+    foreach (SECTION ${KATESTROPHE_SECTIONS})
+        string(TOLOWER ${SECTION} SECTION_LOWERCASE)
+        set(THIS_TARGETS_NAME "${KATESTROPHE_TARGET}.${SECTION_LOWERCASE}")
+
+        # Add the executable and link the libraries.
+        add_executable(${THIS_TARGETS_NAME} ${KATESTROPHE_FILES})
+        target_link_libraries(${THIS_TARGETS_NAME} PUBLIC gtest ${KATESTROPHE_LIBRARIES})
+
+        # Select the correct section of the target by setting the appropriate preprocessor define.
+        target_compile_definitions(${THIS_TARGETS_NAME} PRIVATE ${SECTION})
+
+        # Exclude the target fromn the "all" target.
+        set_target_properties(${THIS_TARGETS_NAME} PROPERTIES EXCLUDE_FROM_ALL TRUE EXCLUDE_FROM_DEFAULT_BUILD TRUE)
+
+        # Add a test invoking "cmake --build" to test if the target compiles.
+        add_test(
+            NAME "${THIS_TARGETS_NAME}"
+            COMMAND cmake --build . --target ${THIS_TARGETS_NAME} --config $<CONFIGURATION>
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        )
+
+        # Specify, that the target should not compile.
+        set_tests_properties("${THIS_TARGETS_NAME}" PROPERTIES WILL_FAIL TRUE)
+    endforeach ()
+endfunction ()
 
 add_library(kamping_test_base INTERFACE)
 if (KAMPING_CXX_FLAGS)
@@ -29,7 +80,7 @@ endfunction ()
 function (kamping_register_test KAMPING_TARGET_NAME)
     cmake_parse_arguments("KAMPING" "NO_GLIBCXX_DEBUG_CONTAINERS" "" "FILES" ${ARGN})
     add_executable(${KAMPING_TARGET_NAME} ${KAMPING_FILES})
-    target_link_libraries(${KAMPING_TARGET_NAME} PRIVATE gtest gtest_main gmock kamping_base kamping_test_base)
+    target_link_libraries(${KAMPING_TARGET_NAME} PRIVATE GTest::gtest_main GTest::gmock kamping_base kamping_test_base)
     gtest_discover_tests(${KAMPING_TARGET_NAME} WORKING_DIRECTORY ${PROJECT_BINARY_DIR} DISCOVERY_MODE PRE_TEST)
     kamping_set_kassert_flags(${KAMPING_TARGET_NAME} ${ARGN})
     if (NOT ${KAMPING_NO_GLIBCXX_DEBUG_CONTAINERS})
