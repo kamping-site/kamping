c366a94c05a7f59fe73b869771c00fc3a7f4a948
Daniel Brommer <daniel.brommer@student.kit.edu>
2025-09-02 10:12:32 +0200

    Add ProbeDataBuffer

---
+++ include/kamping/data_buffers/probe_db.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+
+#include "kamping/communicator.hpp"
+#include "kamping/p2p/probe.hpp"
+
+template <typename T>
+class ProbeDataBuffer {
+public:
+    explicit ProbeDataBuffer(int source) : _data(std::vector<T>()), _source(source) {}
+
+    using value_type = T;
+
+    auto begin() noexcept {
+        return _data.begin();
+    }
+
+    auto end() noexcept {
+        return _data.end();
+    }
+
+    T* data() noexcept {
+        return _data.data();
+    }
+
+    template <typename Communicator>
+    auto set_size(Communicator& comm) {
+        auto status = comm.probe(kamping::status_out()).extract_status();
+        _size = kamping::asserting_cast<size_t>(status.template count_signed<int>());
+    }
+
+    [[nodiscard]] size_t size() noexcept {
+        size_t curr_size = _data.size();
+        if (curr_size != _size) {
+            resize();
+        }
+        return _size;
+    }
+
+    void resize() {
+        _data.resize(_size);
+    }
+
+    [[nodiscard]] size_t size() const noexcept {
+        return _data.size();
+    }
+
+    [[nodiscard]] int source() const noexcept {return _source;}
+
+private:
+    std::vector<T> _data;
+    int _source;
+    size_t         _size = 0;
+};
