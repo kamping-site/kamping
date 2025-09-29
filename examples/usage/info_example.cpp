d430b3fca4d7652745e43229786c9f3c789ded34
Niklas Uhl <uhl@kit.edu>
2025-09-29 17:00:45 -0400

    Disable auto-conversion overload from Info::set.

--- examples/usage/info_example.cpp
+++ examples/usage/info_example.cpp
@@ -1,14 +1,16 @@
 #include <ios>
-#include "kamping/environment.hpp"
+// #include "kamping/environment.hpp"
 #include "kamping/info.hpp"
 
 int main(int argc, char* argv[]) {
-    kamping::Environment env;
+    // kamping::Environment env;
 
     kamping::Info info;
     std::cout << std::boolalpha;
     std::cout << "contains? " << info.contains("foobar") << "\n";
     info.set("foobar", true);
+    info.set("foobar_num", 42);
+    info.set("foobar_string", "hello");
     std::cout << "value: " << info.get("foobar").value_or("INVALID") << "\n";
     return 0;
 }
