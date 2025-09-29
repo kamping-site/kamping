002221e49bc8a2b1a9e0f448a28fc68ccc9b0a40
Niklas Uhl <uhl@kit.edu>
2025-09-29 14:36:33 -0400

    Extend example.

--- examples/usage/info_example.cpp
+++ examples/usage/info_example.cpp
@@ -1,3 +1,4 @@
+#include <ios>
 #include "kamping/environment.hpp"
 #include "kamping/info.hpp"
 
@@ -5,5 +6,9 @@ int main(int argc, char* argv[]) {
     kamping::Environment env;
 
     kamping::Info info;
+    std::cout << std::boolalpha;
+    std::cout << "contains? " << info.contains("foobar") << "\n";
+    info.set("foobar", true);
+    std::cout << "value: " << info.get("foobar").value_or("INVALID") << "\n";
     return 0;
 }
