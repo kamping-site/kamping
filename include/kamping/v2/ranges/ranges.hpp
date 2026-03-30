71b93eb605155616eb6f3cf486d153c9c4e7b1ea
Niklas Uhl <uhl@kit.edu>
2026-03-30 16:36:24 +0200

    Remove `kamping::ranges::mpi_enabled` knob.
    Since the member functions are rather MPI specific now, their
    existence is enough.

--- include/kamping/v2/ranges/ranges.hpp
+++ include/kamping/v2/ranges/ranges.hpp
@@ -16,27 +16,21 @@ concept integer_like = std::integral<T> && !std::same_as<T, bool>;
 template <class T>
 concept ptr_to_object = std::is_pointer_v<T> && std::is_object_v<std::remove_pointer_t<T>>;
 
-template <typename>
-inline constexpr bool mpi_enabled = false;
-
 template <typename T>
-concept is_mpi_enabled = kamping::ranges::mpi_enabled<std::remove_cvref_t<T>>;
-
-template <typename T>
-concept has_mpi_compatible_size_member = is_mpi_enabled<T> && requires(std::remove_reference_t<T> const& t) {
+concept has_mpi_compatible_size_member = requires(std::remove_reference_t<T> const& t) {
     { t.mpi_size() } -> integer_like;
 };
 
 template <typename T>
-concept has_mpi_compatible_data_member = is_mpi_enabled<T> && requires(T&& t) {
+concept has_mpi_compatible_data_member = requires(T&& t) {
     { t.mpi_data() } -> ptr_to_object;
 };
 
 template <typename T>
-concept has_mpi_compatible_type_member = is_mpi_enabled<T> && requires(std::remove_reference_t<T> const& t) {
+concept has_mpi_compatible_type_member = requires(std::remove_reference_t<T> const& t) {
     { t.mpi_type() } -> std::convertible_to<MPI_Datatype>;
 };
-  
+
 template <typename T>
 concept range_of_builtin_mpi_type =
     std::ranges::range<T> && kamping::is_builtin_type_v<std::remove_cvref_t<std::ranges::range_value_t<T>>>;
